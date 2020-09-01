29dbc5031775295094698000d889277794e566df
Yevhenii Molodyi <eugmolody@gmail.com>
2020-09-01 18:03:38 +0300

    Update html-webpack-plugin

--- plugins/post-processing.js
+++ plugins/post-processing.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const postProcessing = prevHtml => {
   let nextHtml;
   try {
-    const str = fs.readFileSync('./dll/stats.json');
-    const stats = JSON.parse(str);
-    const lib = stats.lib;
+    const str = fs.readFileSync('./dll/stats.json')
+    , stats = JSON.parse(str)
+    , lib = stats.lib;    
     nextHtml = prevHtml.replace(
-      '<!--lib-->',
-      '<script src="app/'+ lib + '"></script>'
+      '<script src="app/lib"></script>',
+      '<script src="app/'+ lib + '" defer></script>'
     );
   } catch(err) {
     console.log(err.message)
