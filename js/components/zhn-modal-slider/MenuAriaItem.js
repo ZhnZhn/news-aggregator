ec6656eff3b3fb106f87ed95f397a28bc8909047
Yevhenii Molodyi <eugmolody@gmail.com>
2021-08-03 17:30:08 +0300

    Replace MenuAriaItem by react custom hooks implementation

--- js/components/zhn-modal-slider/MenuAriaItem.js
+++ js/components/zhn-modal-slider/MenuAriaItem.js
@@ -5,80 +5,46 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
-
-var _objectWithoutPropertiesLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutPropertiesLoose"));
-
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
 var _react = require("react");
+
+var _useRegRef = _interopRequireDefault(require("../hooks/useRegRef"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var _fKeyPressed = function _fKeyPressed(onClick) {
-  return function (evt) {
+var MenuAriaItem = function MenuAriaItem(_ref2) {
+  var className = _ref2.className,
+      style = _ref2.style,
+      children = _ref2.children,
+      onReg = _ref2.onReg,
+      onClick = _ref2.onClick;
+
+  var _ref = (0, _react.useRef)(null)
+  /*eslint-disable react-hooks/exhaustive-deps */
+  ,
+      _hKeyPress = (0, _react.useCallback)(function (evt) {
     evt.preventDefault();
     var which = evt.which;
 
     if (which === 13 || which === 32) {
       onClick();
     }
-  };
+  }, []); //onClick
+
+  /*eslint-enable react-hooks/exhaustive-deps */
+
+
+  (0, _useRegRef["default"])(onReg, _ref);
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    ref: onReg ? _ref : void 0,
+    className: className,
+    style: style,
+    role: "menuitem",
+    tabIndex: "0",
+    onClick: onClick,
+    onKeyPress: _hKeyPress,
+    children: children
+  });
 };
-
-var MenuAriaItem = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(MenuAriaItem, _Component);
-
-  function MenuAriaItem() {
-    var _this;
-
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
-
-    _this._ref = function (n) {
-      return _this._node = n;
-    };
-
-    return _this;
-  }
-
-  var _proto = MenuAriaItem.prototype;
-
-  /*
-  static propTypes = {
-    onClick: PropTypes.func,
-    onReg: PropTypes.func
-  }
-  */
-  _proto.componentDidMount = function componentDidMount() {
-    var onReg = this.props.onReg;
-
-    if (this._node && typeof onReg === 'function') {
-      onReg(this._node);
-    }
-  };
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        children = _this$props.children,
-        onClick = _this$props.onClick,
-        onReg = _this$props.onReg,
-        rest = (0, _objectWithoutPropertiesLoose2["default"])(_this$props, ["children", "onClick", "onReg"]);
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", (0, _extends2["default"])({}, rest, {
-      ref: onReg ? this._ref : void 0,
-      role: "menuitem",
-      tabIndex: "0",
-      onClick: onClick,
-      onKeyPress: _fKeyPressed(onClick),
-      children: children
-    }));
-  };
-
-  return MenuAriaItem;
-}(_react.Component);
 
 var _default = MenuAriaItem;
 exports["default"] = _default;
--- src/components/zhn-modal-slider/MenuAriaItem.jsx
+++ src/components/zhn-modal-slider/MenuAriaItem.jsx
@@ -1,46 +1,40 @@
-import { Component } from 'react'
+import { useRef, useCallback } from 'react';
+import useRegRef from '../hooks/useRegRef';
 
-const _fKeyPressed = (onClick) => (evt) => {
-  evt.preventDefault()
-  const { which } = evt;
-  if (which === 13 || which === 32) {
-    onClick()
-  }
-}
-
-class MenuAriaItem extends Component {
-
-  /*
-  static propTypes = {
-    onClick: PropTypes.func,
-    onReg: PropTypes.func
-  }
-  */
-
-  componentDidMount(){
-    const { onReg } = this.props
-    if (this._node && typeof onReg === 'function') {
-      onReg(this._node)
+const MenuAriaItem = ({
+  className,
+  style,
+  children,
+  onReg,
+  onClick
+}) => {
+  const _ref = useRef(null)
+  /*eslint-disable react-hooks/exhaustive-deps */
+  , _hKeyPress = useCallback((evt)=>{
+    evt.preventDefault()
+    const { which } = evt;
+    if (which === 13 || which === 32) {
+      onClick()
     }
-  }
+  }, []);
+  //onClick
+  /*eslint-enable react-hooks/exhaustive-deps */
 
-  _ref = n => this._node = n
+  useRegRef(onReg, _ref)
 
-  render(){
-    const { children, onClick, onReg, ...rest } = this.props
-    return (
-      <div
-        {...rest}
-        ref={onReg ? this._ref: void 0}
-        role="menuitem"
-        tabIndex="0"
-        onClick={onClick}
-        onKeyPress={_fKeyPressed(onClick)}
-      >
-        {children}
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      ref={onReg ? _ref : void 0}
+      className={className}
+      style={style}      
+      role="menuitem"
+      tabIndex="0"
+      onClick={onClick}
+      onKeyPress={_hKeyPress}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default MenuAriaItem
