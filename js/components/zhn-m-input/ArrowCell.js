86af137e9ac91f4cce50048805791479f06b4c1d
Yevhenii Molodyi <eugmolody@gmail.com>
2021-08-17 11:19:14 +0300

    Edit ArrowCell

--- js/components/zhn-m-input/ArrowCell.js
+++ js/components/zhn-m-input/ArrowCell.js
@@ -5,24 +5,22 @@ exports["default"] = void 0;
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var S = {
+/*
+const S = {
   SVG: {
     display: 'inline-block',
     color: 'black',
-    //color: 'rgb(255, 255, 255)',
-    //fill: 'inherit',
     fill: 'black',
     height: '24px',
     width: '24px',
     userSelect: 'none',
     transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms'
   }
-};
-
+}
+*/
 var ArrowCell = function ArrowCell() {
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("svg", {
     viewBox: "0 0 24 24",
-    style: S.ROOT,
     children: /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
       d: "M7 10l5 5 5-5z"
     })
--- src/components/zhn-m-input/ArrowCell.jsx
+++ src/components/zhn-m-input/ArrowCell.jsx
@@ -1,9 +1,8 @@
+/*
 const S = {
   SVG: {
     display: 'inline-block',
     color: 'black',
-    //color: 'rgb(255, 255, 255)',
-    //fill: 'inherit',
     fill: 'black',
     height: '24px',
     width: '24px',
@@ -11,10 +10,11 @@ const S = {
     transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms'
   }
 }
+*/
 
 const ArrowCell = () => (
-  <svg viewBox="0 0 24 24" style={S.ROOT}>
-    <path d="M7 10l5 5 5-5z" />    
+  <svg viewBox="0 0 24 24">
+    <path d="M7 10l5 5 5-5z" />
   </svg>
 );
 
