d58495e71800e1d9ad71a957ac940d162e15896c
Yevhenii Molodyi <eugmolody@gmail.com>
2021-08-18 12:13:09 +0300

    Add crCn to ScrollPane

--- js/components/zhn-atoms/ScrollPane.js
+++ js/components/zhn-atoms/ScrollPane.js
@@ -1,17 +1,25 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 exports.__esModule = true;
 exports["default"] = void 0;
 
+var _crCn = _interopRequireDefault(require("../zhn-utils/crCn"));
+
 var _jsxRuntime = require("react/jsx-runtime");
+
+var CL_WITH_SCROLL = 'with-scroll';
 
 var ScrollPane = function ScrollPane(_ref) {
   var style = _ref.style,
-      _ref$className = _ref.className,
-      className = _ref$className === void 0 ? "" : _ref$className,
+      className = _ref.className,
       children = _ref.children;
+
+  var _cn = (0, _crCn["default"])(CL_WITH_SCROLL, className);
+
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-    className: "with-scroll " + className,
+    className: _cn,
     style: style,
     children: children
   });
--- src/components/zhn-atoms/ScrollPane.jsx
+++ src/components/zhn-atoms/ScrollPane.jsx
@@ -1,7 +1,18 @@
-const ScrollPane = ({ style, className="", children }) => (
-   <div className={`with-scroll ${className}`} style={style}>
-      {children}
-   </div>
-);
+import crCn from '../zhn-utils/crCn';
+
+const CL_WITH_SCROLL = 'with-scroll';
+
+const ScrollPane = ({
+  style,
+  className,
+  children
+}) => {
+  const _cn = crCn(CL_WITH_SCROLL, className);
+  return (
+     <div className={_cn} style={style}>
+        {children}
+     </div>
+  );
+};
 
 export default ScrollPane
