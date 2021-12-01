fc51095a150eab2fbe65131200715165863a384d
Yevhenii Molodyi <eugmolody@gmail.com>
2021-12-01 11:04:51 +0200

    Edit Step style

--- js/components/about/Step.js
+++ js/components/about/Step.js
@@ -5,37 +5,35 @@ exports["default"] = void 0;
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var S = {
-  P: {
-    marginTop: 3
-  },
-  STEP: {
-    display: 'inline-block',
-    color: '#80c040',
-    width: 26,
-    height: 26,
-    lineHeight: '24px',
-    textAlign: 'center',
-    textTransform: 'uppercase',
-    border: '2px solid #80c040',
-    borderRadius: '50%',
-    fontSize: '1.125rem'
-  },
-  DESCR: {
-    color: 'black'
-  }
+var S_P = {
+  marginTop: 3
+},
+    S_STEP = {
+  display: 'inline-block',
+  color: '#80c040',
+  width: 26,
+  height: 26,
+  lineHeight: '24px',
+  textAlign: 'center',
+  textTransform: 'uppercase',
+  border: '2px solid #80c040',
+  borderRadius: '50%',
+  fontSize: '1.125rem'
+},
+    S_DESCR = {
+  color: 'black'
 };
 
 var Step = function Step(_ref) {
   var step = _ref.step,
       description = _ref.description;
   return /*#__PURE__*/(0, _jsxRuntime.jsxs)("p", {
-    style: S.P,
+    style: S_P,
     children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-      style: S.STEP,
+      style: S_STEP,
       children: step
     }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("span", {
-      style: S.DESCR,
+      style: S_DESCR,
       children: ["\xA0", description]
     })]
   });
--- src/components/about/Step.jsx
+++ src/components/about/Step.jsx
@@ -1,33 +1,27 @@
-const S = {
-  P: {
-    marginTop: 3,
-  },
-  STEP: {
-    display: 'inline-block',
-    color: '#80c040',
-    width: 26,
-    height: 26,
-    lineHeight: '24px',
-    textAlign: 'center',
-    textTransform: 'uppercase',
-    border: '2px solid #80c040',
-    borderRadius: '50%',
-    fontSize: '1.125rem'
-  },
-  DESCR: {
-    color: 'black'
+const S_P = { marginTop: 3 }
+, S_STEP = {
+   display: 'inline-block',
+   color: '#80c040',
+   width: 26,
+   height: 26,
+   lineHeight: '24px',
+   textAlign: 'center',
+   textTransform: 'uppercase',
+   border: '2px solid #80c040',
+   borderRadius: '50%',
+   fontSize: '1.125rem'
   }
-};
+  , S_DESCR = { color: 'black' };
 
 const Step = ({
   step,
   description
 }) => (
-  <p style={S.P}>
-    <span style={S.STEP}>
+  <p style={S_P}>
+    <span style={S_STEP}>
       {step}
     </span>
-    <span style={S.DESCR}>
+    <span style={S_DESCR}>
       &nbsp;{description}
     </span>
  </p>
