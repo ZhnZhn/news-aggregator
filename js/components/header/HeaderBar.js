9cf1b91554ce7674c16796c9e4985f438eab231b
Yevhenii Molodyi <eugmolody@gmail.com>
2021-12-08 14:34:19 +0200

    Replace HeaderBar by function component

--- js/components/header/HeaderBar.js
+++ js/components/header/HeaderBar.js
@@ -5,11 +5,9 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
 var _react = require("react");
 
-var _withTheme = _interopRequireDefault(require("../hoc/withTheme"));
+var _useTheme = _interopRequireDefault(require("../hooks/useTheme"));
 
 var _HeaderBar = _interopRequireDefault(require("./HeaderBar.Style"));
 
@@ -40,161 +38,132 @@ var TITLE = "News Aggregator v0.4.0",
   verticalAlign: 'middle',
   margin: '0 8px'
 };
+/*eslint-disable react-hooks/exhaustive-deps */
 
-var _ffClick = function _ffClick(onClose) {
-  return function (onClick) {
-    return function () {
-      onClick();
-      onClose();
-    };
-  };
+var _useClickItem = function _useClickItem(onClick, onClose) {
+  return (0, _react.useCallback)(function () {
+    onClick();
+    onClose();
+  }, []);
+}; // onClick, onClose
+
+/*eslint-enable react-hooks/exhaustive-deps */
+
+
+var HeaderBar = function HeaderBar(_ref) {
+  var store = _ref.store,
+      onNewsSources = _ref.onNewsSources,
+      onSettings = _ref.onSettings,
+      onAbout = _ref.onAbout,
+      onWebz = _ref.onWebz,
+      onWebzCountry = _ref.onWebzCountry,
+      onStackTagged = _ref.onStackTagged,
+      onStackSearch = _ref.onStackSearch,
+      onCryptoCompare = _ref.onCryptoCompare,
+      onCoinStats = _ref.onCoinStats,
+      onMessari = _ref.onMessari,
+      onIex = _ref.onIex,
+      onFmp = _ref.onFmp,
+      onNewsSearch = _ref.onNewsSearch,
+      onNewsTop = _ref.onNewsTop;
+
+  var _useState = (0, _react.useState)(false),
+      isQuery = _useState[0],
+      setIsQuery = _useState[1],
+      _hCloseQuery = (0, _react.useCallback)(function () {
+    return setIsQuery(false);
+  }, []),
+      _hToggleQuery = (0, _react.useCallback)(function () {
+    return setIsQuery(function (is) {
+      return !is;
+    });
+  }, []),
+      _hWebz = _useClickItem(onWebz, _hCloseQuery),
+      _hWebzCountry = _useClickItem(onWebzCountry, _hCloseQuery),
+      _hStackTagged = _useClickItem(onStackTagged, _hCloseQuery),
+      _hStackSearch = _useClickItem(onStackSearch, _hCloseQuery),
+      _hCryptoCompare = _useClickItem(onCryptoCompare, _hCloseQuery),
+      _hCoinStats = _useClickItem(onCoinStats, _hCloseQuery),
+      _hMessari = _useClickItem(onMessari, _hCloseQuery),
+      _hIex = _useClickItem(onIex, _hCloseQuery),
+      _hFmp = _useClickItem(onFmp, _hCloseQuery),
+      _hNewsApi = _useClickItem(onNewsSearch, _hCloseQuery),
+      _hNewsTop = _useClickItem(onNewsTop, _hCloseQuery),
+      TS = (0, _useTheme["default"])(_HeaderBar["default"]);
+
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    className: CL_HEADER,
+    style: TS.HEADER,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_PanelQuery["default"], {
+      paneStyle: TS.PANE,
+      className: CL_PANEL_BROWSER,
+      isShow: isQuery,
+      onClose: _hToggleQuery,
+      onWebz: _hWebz,
+      onWebzCountry: _hWebzCountry,
+      onStackTagged: _hStackTagged,
+      onStackSearch: _hStackSearch,
+      onCryptoCompare: _hCryptoCompare,
+      onCoinStats: _hCoinStats,
+      onMessari: _hMessari,
+      onIex: _hIex,
+      onFmp: _hFmp,
+      onNewsApi: _hNewsApi,
+      onNewsTop: _hNewsTop
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_LoadingProgress["default"], {
+      store: store
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_IconAppLogo["default"], {
+      className: CL_ICON_APP,
+      title: TITLE
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_AppLabel["default"], {
+      className: CL_LABEL_APP,
+      caption: TITLE
+    }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("span", {
+      className: CL_BROWSER_BTS,
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].FlatButton, {
+        style: TS.BT.FLAT_ROOT,
+        clDiv: TS.BT.CL_FLAT_DIV,
+        caption: "News",
+        title: "Open News Sources Browser",
+        accessKey: "n",
+        onClick: onNewsSources
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].ModalButton, {
+        style: TS.BT.FLAT_ROOT,
+        clDiv: TS.BT.CL_FLAT_DIV,
+        caption: "Query",
+        title: "Panel Query Source",
+        accessKey: "q",
+        onClick: _hToggleQuery,
+        children: /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+          className: CL_ARROW_DOWN
+        })
+      })]
+    }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+      className: CL_BTS,
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].FlatButton, {
+        className: CL_BT_ABOUT,
+        style: TS.BT.FLAT_ROOT,
+        clDiv: TS.BT.CL_FLAT_DIV,
+        title: "About News Aggregator",
+        accessKey: "a",
+        onClick: onAbout,
+        children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].SvgInfo, {
+          style: S_SVG_ICON
+        })
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].FlatButton, {
+        style: TS.BT.FLAT_ROOT,
+        clDiv: TS.BT.CL_FLAT_DIV,
+        title: "Open Settings Dialog",
+        accessKey: "s",
+        onClick: onSettings,
+        children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].SvgSettings, {
+          style: S_SVG_ICON
+        })
+      })]
+    })]
+  });
 };
 
-var HeaderBar = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(HeaderBar, _Component);
-
-  function HeaderBar(props) {
-    var _this;
-
-    _this = _Component.call(this, props) || this;
-
-    _this._onRegQuery = function (node) {
-      _this.btQueryNode = node;
-    };
-
-    _this._hToggleQuery = function () {
-      _this.setState(function (prevState) {
-        return {
-          isQuery: !prevState.isQuery
-        };
-      });
-    };
-
-    _this._hCloseQuery = function (event) {
-      _this.setState({
-        isQuery: false
-      });
-    };
-
-    var onWebz = props.onWebz,
-        onWebzCountry = props.onWebzCountry,
-        onStackTagged = props.onStackTagged,
-        onStackSearch = props.onStackSearch,
-        onCryptoCompare = props.onCryptoCompare,
-        onCoinStats = props.onCoinStats,
-        onMessari = props.onMessari,
-        onIex = props.onIex,
-        onFmp = props.onFmp,
-        onNewsSearch = props.onNewsSearch,
-        onNewsTop = props.onNewsTop,
-        _fClick = _ffClick(_this._hCloseQuery);
-
-    _this._hWebz = _fClick(onWebz);
-    _this._hWebzCountry = _fClick(onWebzCountry);
-    _this._hStackTagged = _fClick(onStackTagged);
-    _this._hStackSearch = _fClick(onStackSearch);
-    _this._hCryptoCompare = _fClick(onCryptoCompare);
-    _this._hCoinStats = _fClick(onCoinStats);
-    _this._hMessari = _fClick(onMessari);
-    _this._hIex = _fClick(onIex);
-    _this._hFmp = _fClick(onFmp);
-    _this._hNewsApi = _fClick(onNewsSearch);
-    _this._hNewsTop = _fClick(onNewsTop);
-    _this.state = {
-      isQuery: false
-    };
-    return _this;
-  }
-
-  var _proto = HeaderBar.prototype;
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        store = _this$props.store,
-        theme = _this$props.theme,
-        onNewsSources = _this$props.onNewsSources,
-        onSettings = _this$props.onSettings,
-        onAbout = _this$props.onAbout,
-        TS = theme.createStyle(_HeaderBar["default"]),
-        isQuery = this.state.isQuery;
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-      className: CL_HEADER,
-      style: TS.HEADER,
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_PanelQuery["default"], {
-        paneStyle: TS.PANE,
-        className: CL_PANEL_BROWSER,
-        isShow: isQuery,
-        onClose: this._hToggleQuery,
-        onWebz: this._hWebz,
-        onWebzCountry: this._hWebzCountry,
-        onStackTagged: this._hStackTagged,
-        onStackSearch: this._hStackSearch,
-        onCryptoCompare: this._hCryptoCompare,
-        onCoinStats: this._hCoinStats,
-        onMessari: this._hMessari,
-        onIex: this._hIex,
-        onFmp: this._hFmp,
-        onNewsApi: this._hNewsApi,
-        onNewsTop: this._hNewsTop
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_LoadingProgress["default"], {
-        store: store
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_IconAppLogo["default"], {
-        className: CL_ICON_APP,
-        title: TITLE
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_AppLabel["default"], {
-        className: CL_LABEL_APP,
-        caption: TITLE
-      }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("span", {
-        className: CL_BROWSER_BTS,
-        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].FlatButton, {
-          style: TS.BT.FLAT_ROOT,
-          clDiv: TS.BT.CL_FLAT_DIV,
-          caption: "News",
-          title: "Open News Sources Browser",
-          accessKey: "n",
-          onClick: onNewsSources
-        }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].ModalButton, {
-          style: TS.BT.FLAT_ROOT,
-          clDiv: TS.BT.CL_FLAT_DIV,
-          caption: "Query",
-          title: "Panel Query Source",
-          accessKey: "q",
-          onClick: this._hToggleQuery,
-          onReg: this._onRegQuery,
-          children: /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-            className: CL_ARROW_DOWN
-          })
-        })]
-      }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-        className: CL_BTS,
-        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].FlatButton, {
-          className: CL_BT_ABOUT,
-          style: TS.BT.FLAT_ROOT,
-          clDiv: TS.BT.CL_FLAT_DIV,
-          title: "About News Aggregator",
-          accessKey: "a",
-          onClick: onAbout,
-          children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].SvgInfo, {
-            style: S_SVG_ICON
-          })
-        }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].FlatButton, {
-          style: TS.BT.FLAT_ROOT,
-          clDiv: TS.BT.CL_FLAT_DIV,
-          title: "Open Settings Dialog",
-          accessKey: "s",
-          onClick: onSettings,
-          children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].SvgSettings, {
-            style: S_SVG_ICON
-          })
-        })]
-      })]
-    });
-  };
-
-  return HeaderBar;
-}(_react.Component);
-
-var _default = (0, _withTheme["default"])(HeaderBar);
-
+var _default = HeaderBar;
 exports["default"] = _default;
 //# sourceMappingURL=HeaderBar.js.map
--- src/components/header/HeaderBar.jsx
+++ src/components/header/HeaderBar.jsx
@@ -1,13 +1,13 @@
-import { Component } from 'react'
+import { useState, useCallback } from 'react';
 
-import withTheme from '../hoc/withTheme'
-import styleConfig from './HeaderBar.Style'
+import useTheme from '../hooks/useTheme';
+import styleConfig from './HeaderBar.Style';
 
-import LoadingProgress from './LoadingProgress'
-import IconAppLogo from './IconAppLogo'
-import AppLabel from './AppLabel'
-import A from '../Comp'
-import PanelQuery from './PanelQuery'
+import LoadingProgress from './LoadingProgress';
+import IconAppLogo from './IconAppLogo';
+import AppLabel from './AppLabel';
+import A from '../Comp';
+import PanelQuery from './PanelQuery';
 
 const TITLE = "News Aggregator v0.4.0"
 
@@ -27,139 +27,121 @@ const TITLE = "News Aggregator v0.4.0"
   margin: '0 8px'
 };
 
-const _ffClick = onClose => onClick => () => {
-  onClick()
-  onClose()
-};
+/*eslint-disable react-hooks/exhaustive-deps */
+const _useClickItem = (onClick, onClose) =>
+ useCallback(() => {
+   onClick()
+   onClose()
+ }, [])
+ // onClick, onClose
+/*eslint-enable react-hooks/exhaustive-deps */
 
-class HeaderBar extends Component {
+const HeaderBar = ({
+  store,
+  onNewsSources,
+  onSettings,
+  onAbout,
+  onWebz,
+  onWebzCountry,
+  onStackTagged,
+  onStackSearch,
+  onCryptoCompare,
+  onCoinStats,
+  onMessari,
+  onIex,
+  onFmp,
+  onNewsSearch,
+  onNewsTop
+}) => {
+  const [isQuery, setIsQuery] = useState(false)
+  , _hCloseQuery = useCallback(() => setIsQuery(false), [])
+  , _hToggleQuery = useCallback(() => setIsQuery(is => !is), [])
+  , _hWebz = _useClickItem(onWebz, _hCloseQuery)
+  , _hWebzCountry = _useClickItem(onWebzCountry, _hCloseQuery)
+  , _hStackTagged = _useClickItem(onStackTagged, _hCloseQuery)
+  , _hStackSearch = _useClickItem(onStackSearch, _hCloseQuery)
+  , _hCryptoCompare = _useClickItem(onCryptoCompare, _hCloseQuery)
+  , _hCoinStats = _useClickItem(onCoinStats, _hCloseQuery)
+  , _hMessari = _useClickItem(onMessari, _hCloseQuery)
+  , _hIex = _useClickItem(onIex, _hCloseQuery)
+  , _hFmp = _useClickItem(onFmp, _hCloseQuery)
+  , _hNewsApi = _useClickItem(onNewsSearch, _hCloseQuery)
+  , _hNewsTop = _useClickItem(onNewsTop, _hCloseQuery)
+  , TS = useTheme(styleConfig)
 
-  constructor(props){
-    super(props)
-
-    const {
-      onWebz, onWebzCountry,
-      onStackTagged, onStackSearch,
-      onCryptoCompare, onCoinStats, onMessari,
-      onIex, onFmp,
-      onNewsSearch, onNewsTop
-    } = props
-    , _fClick = _ffClick(this._hCloseQuery);
-
-    this._hWebz = _fClick(onWebz)
-    this._hWebzCountry = _fClick(onWebzCountry)
-    this._hStackTagged = _fClick(onStackTagged)
-    this._hStackSearch = _fClick(onStackSearch)
-    this._hCryptoCompare = _fClick(onCryptoCompare)
-    this._hCoinStats = _fClick(onCoinStats)
-    this._hMessari = _fClick(onMessari)
-    this._hIex = _fClick(onIex)
-    this._hFmp = _fClick(onFmp)
-    this._hNewsApi = _fClick(onNewsSearch)
-    this._hNewsTop = _fClick(onNewsTop)
-
-    this.state = {
-      isQuery: false
-    }
-  }
-
-  _onRegQuery = (node) => {
-    this.btQueryNode = node
-  }
-  _hToggleQuery = () => {
-    this.setState(prevState => ({
-      isQuery: !prevState.isQuery
-    }))
-  }
-  _hCloseQuery = (event) => {
-    this.setState({ isQuery: false })
-  }
-
-  render() {
-    const {
-        store,
-        theme,
-        onNewsSources,
-        onSettings, onAbout
-      } = this.props
-    , TS = theme.createStyle(styleConfig)
-    , { isQuery } = this.state;
-    return (
-      <div className={CL_HEADER} style={TS.HEADER}>
-        <PanelQuery
-          paneStyle={TS.PANE}
-          className={CL_PANEL_BROWSER}
-          isShow={isQuery}
-          onClose={this._hToggleQuery}
-          onWebz={this._hWebz}
-          onWebzCountry={this._hWebzCountry}
-          onStackTagged={this._hStackTagged}
-          onStackSearch={this._hStackSearch}
-          onCryptoCompare={this._hCryptoCompare}
-          onCoinStats={this._hCoinStats}
-          onMessari={this._hMessari}
-          onIex={this._hIex}
-          onFmp={this._hFmp}
-          onNewsApi={this._hNewsApi}
-          onNewsTop={this._hNewsTop}
+  return (
+    <div className={CL_HEADER} style={TS.HEADER}>
+      <PanelQuery
+        paneStyle={TS.PANE}
+        className={CL_PANEL_BROWSER}
+        isShow={isQuery}
+        onClose={_hToggleQuery}
+        onWebz={_hWebz}
+        onWebzCountry={_hWebzCountry}
+        onStackTagged={_hStackTagged}
+        onStackSearch={_hStackSearch}
+        onCryptoCompare={_hCryptoCompare}
+        onCoinStats={_hCoinStats}
+        onMessari={_hMessari}
+        onIex={_hIex}
+        onFmp={_hFmp}
+        onNewsApi={_hNewsApi}
+        onNewsTop={_hNewsTop}
+      />
+      <LoadingProgress
+         store={store}
+      />
+      <IconAppLogo
+         className={CL_ICON_APP}
+         title={TITLE}
+      />
+      <AppLabel
+         className={CL_LABEL_APP}
+         caption={TITLE}
+      />
+      <span className={CL_BROWSER_BTS}>
+        <A.FlatButton
+          style={TS.BT.FLAT_ROOT}
+          clDiv={TS.BT.CL_FLAT_DIV}
+          caption="News"
+          title="Open News Sources Browser"
+          accessKey="n"
+          onClick={onNewsSources}
         />
-        <LoadingProgress
-           store={store}           
-        />
-        <IconAppLogo
-           className={CL_ICON_APP}
-           title={TITLE}
-        />
-        <AppLabel
-           className={CL_LABEL_APP}
-           caption={TITLE}
-        />
-        <span className={CL_BROWSER_BTS}>
+        <A.ModalButton
+           style={TS.BT.FLAT_ROOT}
+           clDiv={TS.BT.CL_FLAT_DIV}
+           caption="Query"
+           title="Panel Query Source"
+           accessKey="q"
+           onClick={_hToggleQuery}
+        >
+          <span className={CL_ARROW_DOWN} />
+        </A.ModalButton>
+      </span>
+      <div className={CL_BTS}>
+          <A.FlatButton
+            className={CL_BT_ABOUT}
+            style={TS.BT.FLAT_ROOT}
+            clDiv={TS.BT.CL_FLAT_DIV}
+            title="About News Aggregator"
+            accessKey="a"
+            onClick={onAbout}
+          >
+            <A.SvgInfo style={S_SVG_ICON} />
+          </A.FlatButton>
           <A.FlatButton
             style={TS.BT.FLAT_ROOT}
             clDiv={TS.BT.CL_FLAT_DIV}
-            caption="News"
-            title="Open News Sources Browser"
-            accessKey="n"
-            onClick={onNewsSources}
-          />
-          <A.ModalButton
-             style={TS.BT.FLAT_ROOT}
-             clDiv={TS.BT.CL_FLAT_DIV}
-             caption="Query"
-             title="Panel Query Source"
-             accessKey="q"
-             onClick={this._hToggleQuery}
-             onReg={this._onRegQuery}
+            title="Open Settings Dialog"
+            accessKey="s"
+            onClick={onSettings}
           >
-            <span className={CL_ARROW_DOWN} />
-          </A.ModalButton>
-        </span>
-        <div className={CL_BTS}>
-            <A.FlatButton
-              className={CL_BT_ABOUT}
-              style={TS.BT.FLAT_ROOT}
-              clDiv={TS.BT.CL_FLAT_DIV}
-              title="About News Aggregator"
-              accessKey="a"
-              onClick={onAbout}
-            >
-              <A.SvgInfo style={S_SVG_ICON} />
-            </A.FlatButton>
-            <A.FlatButton
-              style={TS.BT.FLAT_ROOT}
-              clDiv={TS.BT.CL_FLAT_DIV}
-              title="Open Settings Dialog"
-              accessKey="s"
-              onClick={onSettings}
-            >
-              <A.SvgSettings style={S_SVG_ICON} />
-            </A.FlatButton>
-        </div>
+            <A.SvgSettings style={S_SVG_ICON} />
+          </A.FlatButton>
       </div>
-     );
-   }
-}
+    </div>
+  );
+};
 
-
-export default withTheme(HeaderBar)
+export default HeaderBar
