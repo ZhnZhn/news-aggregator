44ec3250772198649b898b45c9e8dda92adc76b7
Yevhenii Molodyi <eugmolody@gmail.com>
2021-12-09 15:41:22 +0200

    Replace ModalDialogContainer by function component

--- js/components/zhn-containers/ModalDialogContainer.js
+++ js/components/zhn-containers/ModalDialogContainer.js
@@ -5,9 +5,11 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
 var _react = require("react");
+
+var _useListen = _interopRequireDefault(require("../hooks/useListen"));
 
 var _ItemStack = _interopRequireDefault(require("../zhn-atoms/ItemStack"));
 
@@ -15,116 +17,90 @@ var _ModalContainer = _interopRequireDefault(require("./ModalContainer"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-//import PropTypes from 'prop-types'
-var _crDialogItem = function _crDialogItem(dialog, index, _ref) {
-  var store = _ref.store,
-      currentDialog = _ref.currentDialog,
-      data = _ref.data,
-      onClose = _ref.onClose;
-  var comp = dialog.comp,
-      type = dialog.type;
-  return /*#__PURE__*/(0, _react.createElement)(comp, {
-    store: store,
-    key: type,
+var _crDialogItem = function _crDialogItem(_ref, index, _ref2) {
+  var Comp = _ref.Comp,
+      type = _ref.type;
+  var store = _ref2.store,
+      currentDialog = _ref2.currentDialog,
+      data = _ref2.data,
+      onClose = _ref2.onClose;
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(Comp, {
     isShow: currentDialog === type,
     data: data[type],
+    store: store,
     onClose: onClose
-  });
+  }, type);
 };
 
-var ModalDialogContainer = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(ModalDialogContainer, _Component);
+var _isStr = function _isStr(str) {
+  return typeof str === 'string';
+};
 
-  function ModalDialogContainer() {
-    var _this;
+var _getModalDialogType = function _getModalDialogType(option) {
+  return (option || {}).modalDialogType;
+};
 
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
+var ModalDialogContainer = function ModalDialogContainer(_ref3) {
+  var store = _ref3.store,
+      router = _ref3.router,
+      showAction = _ref3.showAction;
 
-    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
-    _this.inits = {};
-    _this.state = {
-      isShow: false,
-      data: {},
-      dialogs: [],
-      currentDialog: null
-    };
+  var _useState = (0, _react.useState)({
+    isShow: false,
+    currentDialog: null,
+    data: {},
+    dialogs: []
+  }),
+      state = _useState[0],
+      setState = _useState[1],
+      isShow = state.isShow,
+      data = state.data,
+      dialogs = state.dialogs,
+      currentDialog = state.currentDialog,
+      _hClose = (0, _react.useCallback)(function () {
+    return setState(function (prevState) {
+      return (0, _extends2["default"])({}, prevState, {
+        isShow: false,
+        currentDialog: null
+      });
+    });
+  }, []);
 
-    _this._onStore = function (actionType, option) {
-      var _this$props = _this.props,
-          router = _this$props.router,
-          showAction = _this$props.showAction;
+  (0, _useListen["default"])(store, function (actionType, option) {
+    if (actionType === showAction) {
+      var type = _getModalDialogType(option);
 
-      if (actionType === showAction) {
-        var type = option.modalDialogType,
-            _this$state = _this.state,
-            data = _this$state.data,
-            dialogs = _this$state.dialogs;
-        data[type] = option;
+      if (_isStr(type)) {
+        setState(function (prevState) {
+          if (!prevState.data[type]) {
+            prevState.dialogs.push({
+              type: type,
+              Comp: router[type]
+            });
+          }
 
-        if (!_this.inits[type]) {
-          dialogs.push({
-            type: type,
-            comp: router[type]
+          prevState.data[type] = option;
+          return (0, _extends2["default"])({}, prevState, {
+            isShow: true,
+            currentDialog: type
           });
-          _this.inits[type] = true;
-        }
-
-        _this.setState({
-          isShow: true,
-          currentDialog: type,
-          data: data,
-          dialogs: dialogs
         });
       }
-    };
-
-    _this._hClose = function () {
-      _this.setState(function (prevState) {
-        return {
-          isShow: false,
-          currentDialog: null
-        };
-      });
-    };
-
-    return _this;
-  }
-
-  var _proto = ModalDialogContainer.prototype;
-
-  _proto.componentDidMount = function componentDidMount() {
-    this.unsubscribe = this.props.store.listen(this._onStore);
-  };
-
-  _proto.componentWillUnmount = function componentWillUnmount() {
-    this.unsubscribe();
-  };
-
-  _proto.render = function render() {
-    var store = this.props.store,
-        _this$state2 = this.state,
-        isShow = _this$state2.isShow,
-        currentDialog = _this$state2.currentDialog,
-        dialogs = _this$state2.dialogs,
-        data = _this$state2.data;
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ModalContainer["default"], {
-      isShow: isShow,
-      onClose: this._hClose,
-      children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_ItemStack["default"], {
-        items: dialogs,
-        crItem: _crDialogItem,
-        store: store,
-        currentDialog: currentDialog,
-        data: data,
-        onClose: this._hClose
-      })
-    });
-  };
-
-  return ModalDialogContainer;
-}(_react.Component);
+    }
+  });
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ModalContainer["default"], {
+    isShow: isShow,
+    onClose: _hClose,
+    children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_ItemStack["default"], {
+      items: dialogs,
+      crItem: _crDialogItem,
+      currentDialog: currentDialog,
+      data: data,
+      store: store,
+      onClose: _hClose
+    })
+  });
+};
 
 var _default = ModalDialogContainer;
 exports["default"] = _default;
--- src/components/zhn-containers/ModalDialogContainer.jsx
+++ src/components/zhn-containers/ModalDialogContainer.jsx
@@ -1,99 +1,85 @@
-import { createElement, Component } from 'react';
-//import PropTypes from 'prop-types'
+import { useState, useCallback } from 'react';
+
+import useListen from '../hooks/useListen';
 
 import ItemStack from '../zhn-atoms/ItemStack';
 import ModalContainer from './ModalContainer';
 
-const _crDialogItem = (dialog, index, {
+const _crDialogItem = (
+  { Comp, type },
+  index, {
   store,
   currentDialog,
   data,
   onClose
+}) => (
+  <Comp
+    key={type}
+    isShow={currentDialog === type}
+    data={data[type]}
+    store={store}
+    onClose={onClose}
+  />
+);
+
+const _isStr = str => typeof str === 'string';
+const _getModalDialogType = option =>
+ (option || {}).modalDialogType;
+
+const ModalDialogContainer = ({
+  store,
+  router,
+  showAction
 }) => {
-  const { comp, type } = dialog;
-  return createElement(comp, {
-    store,
-    key: type,
-    isShow: currentDialog === type,
-    data: data[type],
-    onClose
-  });
+  const [state, setState] = useState({
+     isShow: false,
+     currentDialog: null,
+     data: {},
+     dialogs: [],
+  })
+  , { isShow, data, dialogs, currentDialog } = state
+  , _hClose = useCallback(() => setState(prevState => ({
+     ...prevState,
+     isShow: false,
+     currentDialog: null
+  })), []);
+
+  useListen(store, (actionType, option)=>{
+    if (actionType === showAction){
+      const type = _getModalDialogType(option);
+      if (_isStr(type)) {
+        setState(prevState => {
+          if (!prevState.data[type]) {
+            prevState.dialogs.push({
+               type, Comp: router[type]
+            });
+          }
+          prevState.data[type] = option;
+          return {
+            ...prevState,
+            isShow: true,
+            currentDialog: type
+          };
+        })
+      }
+    }
+  })
+
+  return (
+    <ModalContainer
+      isShow={isShow}
+      onClose={_hClose}
+    >
+      <ItemStack
+        items={dialogs}
+        crItem={_crDialogItem}
+        currentDialog={currentDialog}
+        data={data}
+        store={store}
+        onClose={_hClose}
+      />
+   </ModalContainer>
+  );
 };
-
-class ModalDialogContainer extends Component {
-  /*
-  static propTypes = {
-    store: PropTypes.shape({
-      listen: PropTypes.func
-    })
-  }
-  */
-  inits = {}
-  state = {
-    isShow: false,
-    data: {},
-    dialogs: [],
-    currentDialog: null
-  }
-
-  componentDidMount(){
-    this.unsubscribe = this.props.store.listen(this._onStore);
-  }
-  componentWillUnmount(){
-    this.unsubscribe();
-  }
-  _onStore = (actionType, option) => {
-     const { router, showAction } = this.props;
-     if (actionType === showAction){
-       const type = option.modalDialogType
-       , { data, dialogs } = this.state;
-       data[type] = option;
-       if (!this.inits[type]) {
-         dialogs.push({ type, comp: router[type] });
-         this.inits[type] = true
-       }
-       this.setState({
-         isShow: true,
-         currentDialog: type,
-         data,
-         dialogs
-       })
-     }
-  }
-
-  _hClose = () => {
-    this.setState(prevState => ({
-      isShow: false,
-      currentDialog: null,
-    }))
-  }
-
-
-  render(){
-    const { store } = this.props
-    , {
-      isShow,
-      currentDialog,
-      dialogs,
-      data
-    } = this.state;
-    return (
-      <ModalContainer
-          isShow={isShow}
-          onClose={this._hClose}
-      >
-         <ItemStack
-           items={dialogs}
-           crItem={_crDialogItem}
-           store={store}
-           currentDialog={currentDialog}
-           data={data}
-           onClose={this._hClose}
-         />
-     </ModalContainer>
-   );
-  }
-}
-
 
 export default ModalDialogContainer
