eee4105fe35e20fc89517b50210813ae34004162
Yevhenii Molodyi <eugmolody@gmail.com>
2022-07-21 13:59:27 +0300

    Edit Browser style

--- js/components/zhn-atoms/Browser.js
+++ js/components/zhn-atoms/Browser.js
@@ -7,12 +7,24 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _ContainerStyle = _interopRequireDefault(require("../styles/ContainerStyle"));
-
-var _jsxRuntime = require("react/jsx-runtime");
+var _jsxRuntime = require("preact/jsx-runtime");
 
 //import PropTypes from 'prop-types'
 var CL_SHOW = 'show-popup',
+    S_BROWSER = {
+  flexShrink: 0,
+  zIndex: 20,
+  backgroundColor: '#4d4d4d',
+  position: 'relative',
+  height: 'calc(100vh - 71px)',
+  minHeight: 500,
+  minWidth: 270,
+  maxWidth: 400,
+  marginLeft: 10,
+  padding: '0px 3px 35px 0px',
+  boxShadow: '1px 4px 6px 1px rgba(0,0,0,0.6)',
+  borderRadius: '4px'
+},
     S_BLOCK = {
   display: 'block'
 },
@@ -29,9 +41,9 @@ var Browser = function Browser(_ref) {
       _cn = _ref2[0],
       _style = _ref2[1];
 
-  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+  return (0, _jsxRuntime.jsx)("div", {
     className: _cn,
-    style: (0, _extends2["default"])({}, _ContainerStyle["default"].browserRootDiv, style, _style),
+    style: (0, _extends2["default"])({}, S_BROWSER, style, _style),
     children: children
   });
 };
--- src/components/zhn-atoms/Browser.jsx
+++ src/components/zhn-atoms/Browser.jsx
@@ -1,8 +1,19 @@
 //import PropTypes from 'prop-types'
-
-import STYLE from '../styles/ContainerStyle';
-
 const CL_SHOW = 'show-popup'
+, S_BROWSER = {
+  flexShrink: 0,
+  zIndex: 20,
+  backgroundColor: '#4d4d4d',
+  position: 'relative',
+  height: 'calc(100vh - 71px)',
+  minHeight: 500,
+  minWidth: 270,
+  maxWidth: 400,
+  marginLeft: 10,
+  padding: '0px 3px 35px 0px',
+  boxShadow: '1px 4px 6px 1px rgba(0,0,0,0.6)',
+  borderRadius: '4px'
+}
 , S_BLOCK = { display: 'block' }
 , S_NONE = { display: 'none' };
 
@@ -17,7 +28,7 @@ const Browser = ({
   return (
      <div
         className={_cn}
-        style={{...STYLE.browserRootDiv, ...style, ..._style}}
+        style={{...S_BROWSER, ...style, ..._style}}
       >
         {children}
      </div>
