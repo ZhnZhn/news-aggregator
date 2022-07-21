86f63caf5d1c945441dcad780d9e3ac13fded698
Yevhenii Molodyi <eugmolody@gmail.com>
2022-07-21 12:47:23 +0300

    Add uiApi to index

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _reactDom = require("react-dom");
+var _uiApi = require("./components/uiApi");
 
 var _AppNewsAggregator = _interopRequireDefault(require("./components/AppNewsAggregator"));
 
-var _jsxRuntime = require("react/jsx-runtime");
+var _jsxRuntime = require("preact/jsx-runtime");
 
-(0, _reactDom.render)( /*#__PURE__*/(0, _jsxRuntime.jsx)(_AppNewsAggregator["default"], {}), document.getElementById('app'));
+(0, _uiApi.render)((0, _jsxRuntime.jsx)(_AppNewsAggregator["default"], {}), document.getElementById('app'));
 //# sourceMappingURL=index.js.map
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import { render } from 'react-dom'
+import { render } from './components/uiApi';
 
 import AppNewsAggregator from './components/AppNewsAggregator'
 
