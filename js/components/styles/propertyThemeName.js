28d800630fe42eb6e5af544933bb99953b10a94b
Yevhenii Molodyi <eugmolody@gmail.com>
2022-10-07 16:07:06 +0300

    Add components style propertyThemeName

---
+++ js/components/styles/propertyThemeName.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+exports.__esModule = true;
+exports.crInitialThemeName = exports.THEME_OPTIONS = exports.THEME_NAME = void 0;
+
+var _localStorageFn = require("../../utils/localStorageFn");
+
+var _setDfItemTo = _interopRequireDefault(require("./setDfItemTo"));
+
+var _LS = require("./LS");
+
+var THEME_NAME = {
+  DF: 'GREY',
+  GREY: 'GREY',
+  WHITE: 'WHITE',
+  SAND: 'SAND'
+};
+exports.THEME_NAME = THEME_NAME;
+var THEME_OPTIONS = [{
+  caption: 'Grey',
+  value: 'GREY'
+}, {
+  caption: 'White',
+  value: 'WHITE'
+}, {
+  caption: 'Sand',
+  value: 'SAND'
+}];
+exports.THEME_OPTIONS = THEME_OPTIONS;
+
+var crInitialThemeName = function crInitialThemeName() {
+  var _readFromLs = (0, _localStorageFn.readFromLs)(_LS.LS_UI_THEME_KEY),
+      themeName = _readFromLs[0],
+      _themeName = themeName && THEME_NAME[themeName] ? themeName : THEME_NAME.DF;
+
+  (0, _setDfItemTo["default"])(THEME_OPTIONS, _themeName, 0);
+  return _themeName;
+};
+
+exports.crInitialThemeName = crInitialThemeName;
+//# sourceMappingURL=propertyThemeName.js.map
---
+++ src/components/styles/propertyThemeName.js
@@ -0,0 +1,28 @@
+import {
+  readFromLs
+} from '../../utils/localStorageFn';
+
+import setDfItemTo from './setDfItemTo';
+import { LS_UI_THEME_KEY } from './LS';
+
+export const THEME_NAME = {
+  DF: 'GREY',
+  GREY: 'GREY',
+  WHITE: 'WHITE',
+  SAND: 'SAND'
+}
+
+export const THEME_OPTIONS = [
+  { caption: 'Grey', value: 'GREY' },
+  { caption: 'White', value: 'WHITE' },
+  { caption: 'Sand', value: 'SAND' }
+]
+
+export const crInitialThemeName = () => {
+  const [themeName] = readFromLs(LS_UI_THEME_KEY)
+  , _themeName = themeName && THEME_NAME[themeName]
+      ? themeName
+      : THEME_NAME.DF
+  setDfItemTo(THEME_OPTIONS, _themeName, 0)
+  return _themeName;
+};
