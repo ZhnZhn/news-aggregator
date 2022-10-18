05911033f925fd868b6ed8b823c41d3d7c0033aa
Yevhenii Molodyi <eugmolody@gmail.com>
2022-10-18 11:43:59 +0300

    Replpace useToggle implementation by useReducer

--- js/components/hooks/useToggle.js
+++ js/components/hooks/useToggle.js
@@ -7,24 +7,16 @@ var _uiApi = require("../uiApi");
 
 var _isBool = function _isBool(v) {
   return typeof v === 'boolean';
+},
+    _initState = function _initState(initialValue) {
+  return !!initialValue;
+},
+    _reducer = function _reducer(state, value) {
+  return _isBool(value) ? value : !state;
 };
 
 var useToggle = function useToggle(initialValue) {
-  var _useState = (0, _uiApi.useState)(function () {
-    return !!initialValue;
-  }),
-      is = _useState[0],
-      setIs = _useState[1];
-
-  return [is, (0, _uiApi.useCallback)(function (v) {
-    if (_isBool(v)) {
-      setIs(v);
-    } else {
-      setIs(function (is) {
-        return !is;
-      });
-    }
-  }, [])];
+  return (0, _uiApi.useReducer)(_reducer, initialValue, _initState);
 };
 
 var _default = useToggle;
--- src/components/hooks/useToggle.js
+++ src/components/hooks/useToggle.js
@@ -1,27 +1,20 @@
-import {
-  useState,
-  useCallback
-} from '../uiApi';
+import { useReducer } from '../uiApi';
 
-const _isBool = v => typeof v === 'boolean';
+const _isBool = v => typeof v === 'boolean'
+, _initState = (initialValue) => !!initialValue
+, _reducer = (
+  state,
+  value
+) => _isBool(value)
+  ? value
+  : !state;
 
 const useToggle = (
   initialValue
-) => {
- const [
-   is,
-   setIs
- ] = useState(() => !!initialValue);
- return [
-   is,
-   useCallback((v) => {
-     if (_isBool(v)) {
-       setIs(v)
-     } else {
-       setIs(is => !is)
-     }
-   }, [])
- ];
-};
+) => useReducer(
+  _reducer,
+  initialValue,
+  _initState
+);
 
 export default useToggle
