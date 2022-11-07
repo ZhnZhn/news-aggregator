80e12aa99a78d883986d32e26e3234efb003da87
Yevhenii Molodyi <eugmolody@gmail.com>
2022-11-07 12:28:34 +0200

    Replace implementation for crId

--- js/utils/crId.js
+++ js/utils/crId.js
@@ -2,9 +2,18 @@
 
 exports.__esModule = true;
 exports["default"] = void 0;
-
-var _nanoid = require("nanoid");
-
-var _default = _nanoid.nanoid;
+var MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER,
+  INITIAL_INDEX = Math.pow(10, 7);
+var i = INITIAL_INDEX,
+  idSuffix = '';
+var crId = function crId() {
+  if (++i === MAX_SAFE_INTEGER) {
+    i = INITIAL_INDEX;
+    idSuffix = '-' + Math.floor(Math.random() * 100);
+  }
+  var _str = i.toString(36);
+  return "" + _str.at(-1) + _str.at(-2) + _str.at(-3) + _str.slice(0, -3) + idSuffix;
+};
+var _default = crId;
 exports["default"] = _default;
 //# sourceMappingURL=crId.js.map
--- src/utils/crId.js
+++ src/utils/crId.js
@@ -1,3 +1,15 @@
-import { nanoid } from 'nanoid';
+const MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER
+, INITIAL_INDEX = 10**7;
+let i = INITIAL_INDEX
+, idSuffix = '';
 
-export default nanoid
+const crId = () => {
+  if (++i === MAX_SAFE_INTEGER) {
+    i = INITIAL_INDEX
+    idSuffix = '-' + Math.floor(Math.random()*100)
+  }
+  const _str = (i).toString(36);
+  return `${_str.at(-1)}${_str.at(-2)}${_str.at(-3)}${_str.slice(0, -3)}${idSuffix}`;
+};
+
+export default crId
