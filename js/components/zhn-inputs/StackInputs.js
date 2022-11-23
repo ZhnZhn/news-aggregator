c3449209cf6a6acf9e2eb035f74f4c72bc6dba9e
Yevhenii Molodyi <eugmolody@gmail.com>
2022-11-23 12:32:44 +0200

    Add TextField function component to StackInputs

--- js/components/zhn-inputs/StackInputs.js
+++ js/components/zhn-inputs/StackInputs.js
@@ -4,13 +4,15 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 var _InputSelect = _interopRequireDefault(require("../zhn-m-input/InputSelect"));
+var _TextField = _interopRequireDefault(require("../zhn-m-input/TextField"));
 var _jsxRuntime = require("preact/jsx-runtime");
 var StackInputs = function StackInputs(_ref) {
   var TS = _ref.TS,
     configs = _ref.configs,
     onSelect = _ref.onSelect;
   return (configs || []).map(function (arrConfig) {
-    if (arrConfig[0] === 's') {
+    var _type = arrConfig[0];
+    if (_type === 's') {
       return (0, _jsxRuntime.jsx)(_InputSelect["default"], {
         id: arrConfig[1],
         caption: arrConfig[2],
@@ -18,6 +20,15 @@ var StackInputs = function StackInputs(_ref) {
         initItem: arrConfig[4],
         styleConfig: TS.SELECT,
         onSelect: onSelect
+      }, arrConfig[1]);
+    } else if (_type === 't') {
+      return (0, _jsxRuntime.jsx)(_TextField["default"], {
+        id: arrConfig[1],
+        style: TS.INPUT_ROOT,
+        caption: arrConfig[2],
+        initValue: arrConfig[3],
+        onEnter: onSelect,
+        onBlur: onSelect
       }, arrConfig[1]);
     }
     return null;
--- src/components/zhn-inputs/StackInputs.jsx
+++ src/components/zhn-inputs/StackInputs.jsx
@@ -1,11 +1,13 @@
 import InputSelect from '../zhn-m-input/InputSelect';
+import TextField from '../zhn-m-input/TextField';
 
 const StackInputs = ({
   TS,
   configs,
   onSelect
 }) => (configs || []).map(arrConfig => {
-  if (arrConfig[0] === 's') {
+  const _type = arrConfig[0];
+  if (_type === 's') {
     return (
       <InputSelect
         key={arrConfig[1]}
@@ -15,6 +17,18 @@ const StackInputs = ({
         initItem={arrConfig[4]}
         styleConfig={TS.SELECT}
         onSelect={onSelect}
+      />
+    );
+  } else if (_type === 't') {
+    return (
+      <TextField
+        key={arrConfig[1]}
+        id={arrConfig[1]}
+        style={TS.INPUT_ROOT}
+        caption={arrConfig[2]}
+        initValue={arrConfig[3]}
+        onEnter={onSelect}
+        onBlur={onSelect}
       />
     );
   }
