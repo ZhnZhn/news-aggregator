34866f80f6f0b012793ab2c5ed5e1815a07755cc
Yevhenii Molodyi <eugmolody@gmail.com>
2022-11-25 14:40:07 +0200

    Add ShowHide to StackInputs function component

--- js/components/zhn-inputs/StackInputs.js
+++ js/components/zhn-inputs/StackInputs.js
@@ -4,37 +4,45 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+var _ShowHide = _interopRequireDefault(require("../zhn-atoms/ShowHide"));
 var _InputSelect = _interopRequireDefault(require("../zhn-m-input/InputSelect"));
 var _TextField = _interopRequireDefault(require("../zhn-m-input/TextField"));
 var _jsxRuntime = require("preact/jsx-runtime");
 var _preact = require("preact");
+var _isObj = function _isObj(v) {
+  return v && typeof v === 'object';
+};
+var _isInput = function _isInput(isInputs, id) {
+  return _isObj(isInputs) ? !!isInputs[id] : true;
+};
 var StackInputs = function StackInputs(_ref) {
   var TS = _ref.TS,
+    isInputs = _ref.isInputs,
     configs = _ref.configs,
     onSelect = _ref.onSelect;
   return (configs || []).map(function (arrConfig) {
-    var _type = arrConfig[0];
-    if (_type === 's') {
-      return (0, _jsxRuntime.jsx)(_InputSelect["default"], {
-        id: arrConfig[1],
+    var _type = arrConfig[0],
+      _inputId = arrConfig[1],
+      _elItem = _type === 's' ? (0, _jsxRuntime.jsx)(_InputSelect["default"], {
+        id: _inputId,
         caption: arrConfig[2],
         options: arrConfig[3],
         initItem: arrConfig[4],
         styleConfig: TS.SELECT,
         onSelect: onSelect
-      }, arrConfig[1]);
-    } else if (_type === 't') {
-      return (0, _preact.createElement)(_TextField["default"], (0, _extends2["default"])({}, arrConfig[4], {
-        key: arrConfig[1],
-        id: arrConfig[1],
+      }, _inputId) : _type === 't' ? (0, _preact.createElement)(_TextField["default"], (0, _extends2["default"])({}, arrConfig[4], {
+        key: _inputId,
+        id: _inputId,
         style: TS.INPUT_ROOT,
         caption: arrConfig[2],
         initValue: arrConfig[3],
         onEnter: onSelect,
         onBlur: onSelect
-      }));
-    }
-    return null;
+      })) : void 0;
+    return _elItem ? (0, _jsxRuntime.jsx)(_ShowHide["default"], {
+      isShow: _isInput(isInputs, _inputId),
+      children: _elItem
+    }) : null;
   });
 };
 var _default = StackInputs;
--- src/components/zhn-inputs/StackInputs.jsx
+++ src/components/zhn-inputs/StackInputs.jsx
@@ -1,39 +1,53 @@
+import ShowHide from '../zhn-atoms/ShowHide';
 import InputSelect from '../zhn-m-input/InputSelect';
 import TextField from '../zhn-m-input/TextField';
 
+const _isObj = v => v && typeof v === 'object';
+
+const _isInput = (
+  isInputs,
+  id
+) => _isObj(isInputs)
+  ? !!isInputs[id]
+  : true;
+
 const StackInputs = ({
   TS,
+  isInputs,
   configs,
   onSelect
 }) => (configs || []).map(arrConfig => {
-  const _type = arrConfig[0];
-  if (_type === 's') {
-    return (
-      <InputSelect
-        key={arrConfig[1]}
-        id={arrConfig[1]}
-        caption={arrConfig[2]}
-        options={arrConfig[3]}
-        initItem={arrConfig[4]}
-        styleConfig={TS.SELECT}
-        onSelect={onSelect}
-      />
-    );
-  } else if (_type === 't') {
-    return (
-      <TextField
-        {...arrConfig[4]}
-        key={arrConfig[1]}
-        id={arrConfig[1]}
-        style={TS.INPUT_ROOT}
-        caption={arrConfig[2]}
-        initValue={arrConfig[3]}
-        onEnter={onSelect}
-        onBlur={onSelect}
-      />
-    );
-  }
-  return null;
+  const _type = arrConfig[0]
+  , _inputId = arrConfig[1]
+  , _elItem = _type === 's'
+      ? (<InputSelect
+           key={_inputId}
+           id={_inputId}
+           caption={arrConfig[2]}
+           options={arrConfig[3]}
+           initItem={arrConfig[4]}
+           styleConfig={TS.SELECT}
+           onSelect={onSelect}
+        />
+      )
+      : _type === 't'
+          ? (<TextField
+               {...arrConfig[4]}
+               key={_inputId}
+               id={_inputId}
+               style={TS.INPUT_ROOT}
+               caption={arrConfig[2]}
+               initValue={arrConfig[3]}
+               onEnter={onSelect}
+               onBlur={onSelect}
+            />
+          )
+          : void 0;
+  return _elItem ? (
+    <ShowHide isShow={_isInput(isInputs, _inputId)}>
+      {_elItem}
+    </ShowHide>
+  ) : null;
 });
 
 export default StackInputs
