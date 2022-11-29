f5d1395215f749d6a9b01c2b8512d9a3ecbba1e3
Yevhenii Molodyi <eugmolody@gmail.com>
2022-11-29 11:16:31 +0200

    Add DialogStackInputs to IexNewsDialog

--- js/components/dialogs/IexNewsDialog.js
+++ js/components/dialogs/IexNewsDialog.js
@@ -5,57 +5,28 @@ exports.__esModule = true;
 exports["default"] = void 0;
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 var _ProviderNames = require("../../conf/ProviderNames");
-var _Dialog = _interopRequireDefault(require("./Dialog.Style"));
-var _useRefInputs2 = _interopRequireDefault(require("./hooks/useRefInputs"));
-var _useDialog2 = _interopRequireDefault(require("./hooks/useDialog"));
-var _useDecorDialog2 = _interopRequireDefault(require("./hooks/useDecorDialog"));
-var _DraggableDialog = _interopRequireDefault(require("../zhn-moleculs/DraggableDialog"));
-var _FlexColumn = _interopRequireDefault(require("../zhn-atoms/FlexColumn"));
-var _StackInputs = _interopRequireDefault(require("../zhn-inputs/StackInputs"));
-var _PoweredBy = _interopRequireDefault(require("../links/PoweredBy"));
+var _DialogStackInputs = _interopRequireDefault(require("./DialogStackInputs"));
 var _Links = require("../links/Links");
 var _DialogFn = require("./DialogFn");
 var _jsxRuntime = require("preact/jsx-runtime");
 var RECENT_OPTIONS = [["10 News", "10"], ["20 News", "20"], ["30 News", "30"], ["40 News", "40"], ["50 News", "50"]],
   DF_RECENT = RECENT_OPTIONS[1],
   DF_SYMBOL = "AAPL";
-var INPUT_CONFIGS = [['t', 'symbol', 'Stock Symbol', DF_SYMBOL, (0, _extends2["default"])({}, _DialogFn.SYMBOL_INPUT_PROPS)], ['s', 'recent', 'Recent', RECENT_OPTIONS, DF_RECENT]],
-  INITIAL_INPUTS = (0, _DialogFn.crDfInputs)(INPUT_CONFIGS)[0];
+var INPUT_CONFIGS = [['t', 'symbol', 'Stock Symbol', DF_SYMBOL, (0, _extends2["default"])({}, _DialogFn.SYMBOL_INPUT_PROPS), true], ['s', 'recent', 'Recent', RECENT_OPTIONS, DF_RECENT, true]],
+  _crDfInputs = (0, _DialogFn.crDfInputs)(INPUT_CONFIGS),
+  INITIAL_INPUTS = _crDfInputs[0],
+  TOGGLES = _crDfInputs[1],
+  IS_INPUTS = _crDfInputs[2];
 var IexNewsDialog = function IexNewsDialog(props) {
-  var isShow = props.isShow,
-    onShow = props.onShow,
-    _useRefInputs = (0, _useRefInputs2["default"])(INITIAL_INPUTS),
-    _refInputs = _useRefInputs[0],
-    _selectInput = _useRefInputs[1],
-    _useDialog = (0, _useDialog2["default"])(props, 'IEX', _refInputs),
-    _refDialog = _useDialog[0],
-    _hLoad = _useDialog[1],
-    _hClose = _useDialog[2],
-    _useDecorDialog = (0, _useDecorDialog2["default"])(_Dialog["default"], _hLoad, _hClose),
-    TS = _useDecorDialog[0],
-    _hKeyDown = _useDecorDialog[1];
-  return (0, _jsxRuntime.jsx)(_DraggableDialog["default"], {
-    ref: _refDialog,
-    isShow: isShow,
-    style: TS.R_DIALOG,
-    captionStyle: TS.BROWSER_CAPTION,
-    buttonStyle: TS.BT,
+  return (0, _jsxRuntime.jsx)(_DialogStackInputs["default"], (0, _extends2["default"])({}, props, {
     caption: _ProviderNames.IEX_CLOUD + ": Stock News",
-    onKeyDown: _hKeyDown,
-    onLoad: _hLoad,
-    onShow: onShow,
-    onClose: _hClose,
-    children: (0, _jsxRuntime.jsxs)(_FlexColumn["default"], {
-      children: [(0, _jsxRuntime.jsx)(_StackInputs["default"], {
-        TS: TS,
-        configs: INPUT_CONFIGS,
-        onSelect: _selectInput
-      }), (0, _jsxRuntime.jsx)(_PoweredBy["default"], {
-        style: TS.POWERED_BY,
-        children: (0, _jsxRuntime.jsx)(_Links.IexApiLink, {})
-      })]
-    })
-  });
+    loadId: "IEX",
+    INPUT_CONFIGS: INPUT_CONFIGS,
+    INITIAL_INPUTS: INITIAL_INPUTS,
+    TOGGLES: TOGGLES,
+    IS_INPUTS: IS_INPUTS,
+    children: (0, _jsxRuntime.jsx)(_Links.IexApiLink, {})
+  }));
 };
 var _default = IexNewsDialog;
 exports["default"] = _default;
--- src/components/dialogs/IexNewsDialog.jsx
+++ src/components/dialogs/IexNewsDialog.jsx
@@ -2,16 +2,7 @@ import {
   IEX_CLOUD
 } from '../../conf/ProviderNames';
 
-import styleConfig from './Dialog.Style';
-
-import useRefInputs from './hooks/useRefInputs';
-import useDialog from './hooks/useDialog';
-import useDecorDialog from './hooks/useDecorDialog';
-
-import DraggableDialog from '../zhn-moleculs/DraggableDialog';
-import FlexColumn from '../zhn-atoms/FlexColumn';
-import StackInputs from '../zhn-inputs/StackInputs';
-import PoweredBy from '../links/PoweredBy';
+import DialogStackInputs from './DialogStackInputs';
 import { IexApiLink } from '../links/Links';
 import {
   SYMBOL_INPUT_PROPS,
@@ -30,55 +21,27 @@ const RECENT_OPTIONS = [
 const INPUT_CONFIGS = [
   ['t','symbol','Stock Symbol',DF_SYMBOL,{
     ...SYMBOL_INPUT_PROPS
-  }],
-  ['s','recent','Recent',RECENT_OPTIONS,DF_RECENT]
+  },true],
+  ['s','recent','Recent',RECENT_OPTIONS,DF_RECENT,true]
 ]
-, INITIAL_INPUTS = crDfInputs(INPUT_CONFIGS)[0];
+, [
+  INITIAL_INPUTS,
+  TOGGLES,
+  IS_INPUTS
+] = crDfInputs(INPUT_CONFIGS);
 
-const IexNewsDialog = (props) => {
-  const {
-    isShow,
-    onShow
-  } = props
-  , [
-    _refInputs,
-    _selectInput
-  ] = useRefInputs(INITIAL_INPUTS)
-  , [
-    _refDialog,
-    _hLoad,
-    _hClose
-  ] = useDialog(props, 'IEX', _refInputs)
-  , [
-    TS,
-    _hKeyDown
-  ] = useDecorDialog(styleConfig, _hLoad, _hClose);
-
-  return (
-    <DraggableDialog
-       ref={_refDialog}
-       isShow={isShow}
-       style={TS.R_DIALOG}
-       captionStyle={TS.BROWSER_CAPTION}
-       buttonStyle={TS.BT}
-       caption={`${IEX_CLOUD}: Stock News`}
-       onKeyDown={_hKeyDown}
-       onLoad={_hLoad}
-       onShow={onShow}
-       onClose={_hClose}
-    >
-       <FlexColumn>
-         <StackInputs
-           TS={TS}
-           configs={INPUT_CONFIGS}
-           onSelect={_selectInput}
-         />
-         <PoweredBy style={TS.POWERED_BY}>
-           <IexApiLink />
-         </PoweredBy>
-      </FlexColumn>
-    </DraggableDialog>
-  );
-};
+const IexNewsDialog = (props) => (
+   <DialogStackInputs
+     {...props}
+     caption={`${IEX_CLOUD}: Stock News`}
+     loadId="IEX"
+     INPUT_CONFIGS={INPUT_CONFIGS}
+     INITIAL_INPUTS={INITIAL_INPUTS}
+     TOGGLES={TOGGLES}
+     IS_INPUTS={IS_INPUTS}
+   >
+     <IexApiLink />
+   </DialogStackInputs>
+);
 
 export default IexNewsDialog
