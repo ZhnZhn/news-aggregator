c7b79d2fa17fb60ce90f38d7c5f15a1e654458d5
Yevhenii Molodyi <eugmolody@gmail.com>
2022-11-29 11:21:51 +0200

    Add DialogStackInputs to FmpNewsDialog

--- js/components/dialogs/FmpNewsDialog.js
+++ js/components/dialogs/FmpNewsDialog.js
@@ -5,55 +5,26 @@ exports.__esModule = true;
 exports["default"] = void 0;
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 var _ProviderNames = require("../../conf/ProviderNames");
-var _Dialog = _interopRequireDefault(require("./Dialog.Style"));
-var _useRefInputs2 = _interopRequireDefault(require("./hooks/useRefInputs"));
-var _useDialog2 = _interopRequireDefault(require("./hooks/useDialog"));
-var _useDecorDialog2 = _interopRequireDefault(require("./hooks/useDecorDialog"));
-var _DraggableDialog = _interopRequireDefault(require("../zhn-moleculs/DraggableDialog"));
-var _FlexColumn = _interopRequireDefault(require("../zhn-atoms/FlexColumn"));
-var _StackInputs = _interopRequireDefault(require("../zhn-inputs/StackInputs"));
-var _PoweredBy = _interopRequireDefault(require("../links/PoweredBy"));
+var _DialogStackInputs = _interopRequireDefault(require("./DialogStackInputs"));
 var _Links = require("../links/Links");
 var _DialogFn = require("./DialogFn");
 var _jsxRuntime = require("preact/jsx-runtime");
 var DF_SYMBOL = "AAPL";
 var INPUT_CONFIGS = [['t', 'symbol', 'Stock Symbol', DF_SYMBOL, (0, _extends2["default"])({}, _DialogFn.SYMBOL_INPUT_PROPS)]],
-  INITIAL_INPUTS = (0, _DialogFn.crDfInputs)(INPUT_CONFIGS)[0];
+  _crDfInputs = (0, _DialogFn.crDfInputs)(INPUT_CONFIGS),
+  INITIAL_INPUTS = _crDfInputs[0],
+  TOGGLES = _crDfInputs[1],
+  IS_INPUTS = _crDfInputs[2];
 var FmpNewsDialog = function FmpNewsDialog(props) {
-  var isShow = props.isShow,
-    onShow = props.onShow,
-    _useRefInputs = (0, _useRefInputs2["default"])(INITIAL_INPUTS),
-    _refInputs = _useRefInputs[0],
-    _selectInput = _useRefInputs[1],
-    _useDialog = (0, _useDialog2["default"])(props, 'FMP', _refInputs),
-    _refDialog = _useDialog[0],
-    _hLoad = _useDialog[1],
-    _hClose = _useDialog[2],
-    _useDecorDialog = (0, _useDecorDialog2["default"])(_Dialog["default"], _hLoad, _hClose),
-    TS = _useDecorDialog[0],
-    _hKeyDown = _useDecorDialog[1];
-  return (0, _jsxRuntime.jsx)(_DraggableDialog["default"], {
-    ref: _refDialog,
-    isShow: isShow,
-    style: TS.R_DIALOG,
-    captionStyle: TS.BROWSER_CAPTION,
-    buttonStyle: TS.BT,
+  return (0, _jsxRuntime.jsx)(_DialogStackInputs["default"], (0, _extends2["default"])({}, props, {
     caption: _ProviderNames.FMP + ": Stock News",
-    onKeyDown: _hKeyDown,
-    onLoad: _hLoad,
-    onShow: onShow,
-    onClose: _hClose,
-    children: (0, _jsxRuntime.jsxs)(_FlexColumn["default"], {
-      children: [(0, _jsxRuntime.jsx)(_StackInputs["default"], {
-        TS: TS,
-        configs: INPUT_CONFIGS,
-        onSelect: _selectInput
-      }), (0, _jsxRuntime.jsx)(_PoweredBy["default"], {
-        style: TS.POWERED_BY,
-        children: (0, _jsxRuntime.jsx)(_Links.FmpApiLink, {})
-      })]
-    })
-  });
+    loadId: "FMP",
+    INPUT_CONFIGS: INPUT_CONFIGS,
+    INITIAL_INPUTS: INITIAL_INPUTS,
+    TOGGLES: TOGGLES,
+    IS_INPUTS: IS_INPUTS,
+    children: (0, _jsxRuntime.jsx)(_Links.FmpApiLink, {})
+  }));
 };
 var _default = FmpNewsDialog;
 exports["default"] = _default;
--- src/components/dialogs/FmpNewsDialog.jsx
+++ src/components/dialogs/FmpNewsDialog.jsx
@@ -1,15 +1,6 @@
 import { FMP } from '../../conf/ProviderNames';
 
-import styleConfig from './Dialog.Style';
-
-import useRefInputs from './hooks/useRefInputs';
-import useDialog from './hooks/useDialog';
-import useDecorDialog from './hooks/useDecorDialog';
-
-import DraggableDialog from '../zhn-moleculs/DraggableDialog';
-import FlexColumn from '../zhn-atoms/FlexColumn';
-import StackInputs from '../zhn-inputs/StackInputs';
-import PoweredBy from '../links/PoweredBy';
+import DialogStackInputs from './DialogStackInputs';
 import { FmpApiLink } from '../links/Links';
 import {
   SYMBOL_INPUT_PROPS,
@@ -22,52 +13,24 @@ const INPUT_CONFIGS = [
     ...SYMBOL_INPUT_PROPS
   }]
 ]
-, INITIAL_INPUTS = crDfInputs(INPUT_CONFIGS)[0];
+, [
+  INITIAL_INPUTS,
+  TOGGLES,
+  IS_INPUTS
+] = crDfInputs(INPUT_CONFIGS);
 
-const FmpNewsDialog = (props) => {
-  const {
-    isShow,
-    onShow
-  } = props
-  , [
-    _refInputs,
-    _selectInput
-  ] = useRefInputs(INITIAL_INPUTS)
-  , [
-    _refDialog,
-    _hLoad,
-    _hClose
-  ] = useDialog(props, 'FMP', _refInputs)
-  , [
-    TS,
-    _hKeyDown
-  ] = useDecorDialog(styleConfig, _hLoad, _hClose);
-
-  return (
-    <DraggableDialog
-       ref={_refDialog}
-       isShow={isShow}
-       style={TS.R_DIALOG}
-       captionStyle={TS.BROWSER_CAPTION}
-       buttonStyle={TS.BT}
-       caption={`${FMP}: Stock News`}
-       onKeyDown={_hKeyDown}
-       onLoad={_hLoad}
-       onShow={onShow}
-       onClose={_hClose}
-    >
-      <FlexColumn>
-         <StackInputs
-           TS={TS}
-           configs={INPUT_CONFIGS}
-           onSelect={_selectInput}
-         />
-         <PoweredBy style={TS.POWERED_BY}>
-           <FmpApiLink />
-         </PoweredBy>
-      </FlexColumn>
-    </DraggableDialog>
-  );
-}
+const FmpNewsDialog = (props) => (
+  <DialogStackInputs
+    {...props}
+    caption={`${FMP}: Stock News`}
+    loadId="FMP"
+    INPUT_CONFIGS={INPUT_CONFIGS}
+    INITIAL_INPUTS={INITIAL_INPUTS}
+    TOGGLES={TOGGLES}
+    IS_INPUTS={IS_INPUTS}
+  >
+    <FmpApiLink />
+  </DialogStackInputs>
+)
 
 export default FmpNewsDialog
