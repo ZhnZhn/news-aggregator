175ab687b333305e5b06831426e7aad3ae5162f2
Yevhenii Molodyi <eugmolody@gmail.com>
2022-11-30 15:35:27 +0200

    Add DialogStackInputs to StackSearchDialog

--- src/components/dialogs/StackSearchDialog.jsx
+++ src/components/dialogs/StackSearchDialog.jsx
@@ -1,25 +1,14 @@
-import {
-  useCallback,
-  getRefValue
-} from '../uiApi';
-
 import DateUtil from '../../utils/dt';
-import styleConfig from './Dialog.Style';
-
-import useRefClose from './hooks/useRefClose';
-import useRefInput from './hooks/useRefInput';
-import useRefSelectOption from './hooks/useRefSelectOption';
-import useDecorDialog from './hooks/useDecorDialog';
-
-import A from '../Comp';
-import { getItemValue } from '../zhn-m-input/OptionFn';
-import InputFromToDate from './InputFromToDate';
-import { PoweredByStackOverflow } from '../links/PoweredByLink';
-import { getDialogCaption } from './DialogFn';
+import DialogStackInputs from './DialogStackInputs';
+import { StackOverflowLink } from '../links/Links';
+import {
+  crTextFieldCaption,
+  crDfInputs,
+  getDialogCaption
+} from './DialogFn';
 
 const INITIAL_FROM_DATE = DateUtil.getFromDate(1)
 , INITIAL_TO_DATE = DateUtil.getToDate()
-, _toUTCSecond = DateUtil.toUTCSecond
 , SORT_BY_OPTIONS = [
   ["Activity, Recent Day", "activity"],
   ["Creation Date", "creation"],
@@ -27,118 +16,35 @@ const INITIAL_FROM_DATE = DateUtil.getFromDate(1)
   ["Relevance", "relevance"],
 ]
 , DF_SORT_BY = SORT_BY_OPTIONS[2]
-, INITIAL_SORT_BY_VALUE = getItemValue(DF_SORT_BY)
 , DF_TAGGED = 'CSS'
 , DF_IN_TITLE = 'flexbox'
-, _crInputTitle = (
-  prefix,
-  dfValue
-) => `${prefix} (Default: ${dfValue})`
+, INPUT_CONFIGS = [
+  ['t','tagged',crTextFieldCaption('Tagged',DF_TAGGED),DF_TAGGED,void 0, true],
+  ['t','inTitle',crTextFieldCaption('In Title',DF_IN_TITLE),DF_IN_TITLE,void 0,true],
+  ['s','sortBy','SortBy',SORT_BY_OPTIONS,DF_SORT_BY],
+  ['ft','_ft','From To Date',INITIAL_FROM_DATE,INITIAL_TO_DATE]
+]
+, [
+  INITIAL_INPUTS,
+  TOGGLES,
+  IS_INPUTS
+] = crDfInputs(INPUT_CONFIGS);
 
-const StackSearchDialog = ({
-  isShow,
-  type,
-  source,
-  itemConf,
-  onLoad,
-  onShow,
-  onClose
-}) => {
-  const [
-    _refDialog,
-    _hClose
-  ] = useRefClose(onClose)
-  , [
-    _refTagged,
-    _getInputTagged
-  ] = useRefInput(DF_TAGGED)
-  , [
-    _refInTitle,
-    _getInputInTitle
-  ] = useRefInput(DF_IN_TITLE)
-  , [
-    _refSortBy,
-    _selectSortBy
-  ] = useRefSelectOption(INITIAL_SORT_BY_VALUE)
-  , [
-    _refFromDate,
-    _getInputFromDate
-  ] = useRefInput(INITIAL_FROM_DATE)
-  , [
-    _refToDate,
-    _getInputToDate
-  ] = useRefInput(INITIAL_TO_DATE)
-  /*eslint-disable react-hooks/exhaustive-deps */
-  , _hLoad = useCallback(()=>{
-      onLoad({
-        type,
-        source,
-        itemConf,
-        loadId: 'SO',
-        requestType: 'SEARCH',
-        tagged: _getInputTagged(),
-        inTitle: _getInputInTitle(),
-        sortBy: getRefValue(_refSortBy),
-        fromDate: _toUTCSecond(_getInputFromDate()),
-        toDate: _toUTCSecond(_getInputToDate())
-      })
-      _hClose()
-  }, [])
-  // type, source, itemConf, _hClose
-  // ...
-  /*eslint-enable react-hooks/exhaustive-deps */
-  , [
-    TS,
-    _hKeyDown
-  ] = useDecorDialog(styleConfig, _hLoad, _hClose)
-  , _titleTagged = _crInputTitle('Tagged', DF_TAGGED)
-  , _titleInTitle = _crInputTitle('In Title', DF_IN_TITLE)
-  , caption = getDialogCaption(itemConf)
-
+const StackSearchDialog = (props) => {
+  const caption = getDialogCaption(props.itemConf);
   return (
-    <A.DraggableDialog
-      ref={_refDialog}
-      isShow={isShow}
-      style={TS.R_DIALOG}
-      captionStyle={TS.BROWSER_CAPTION}
-      buttonStyle={TS.BT}
+    <DialogStackInputs
+      {...props}
       caption={caption}
-      onKeyDown={_hKeyDown}
-      onLoad={_hLoad}
-      onShow={onShow}
-      onClose={_hClose}
+      loadId="SO"
+      INPUT_CONFIGS={INPUT_CONFIGS}
+      INITIAL_INPUTS={INITIAL_INPUTS}
+      TOGGLES={TOGGLES}
+      IS_INPUTS={IS_INPUTS}
     >
-       <A.TextField
-         style={TS.INPUT_ROOT}
-         ref={_refTagged}
-         caption={_titleTagged}
-         initValue={DF_TAGGED}
-       />
-       <A.TextField
-         style={TS.INPUT_ROOT}
-         ref={_refInTitle}
-         caption={_titleInTitle}
-         initValue={DF_IN_TITLE}
-       />
-       <A.InputSelect
-         caption="SortBy"
-         initItem={DF_SORT_BY}
-         options={SORT_BY_OPTIONS}
-         styleConfig={TS.SELECT}
-         onSelect={_selectSortBy}
-       />
-       <InputFromToDate
-         refFromDate={_refFromDate}
-         refToDate={_refToDate}
-         TS={TS}
-         initialFrom={INITIAL_FROM_DATE}
-         initialTo={INITIAL_TO_DATE}
-       />
-      <PoweredByStackOverflow
-         style={TS.POWERED_BY}
-      />
-    </A.DraggableDialog>
+      <StackOverflowLink />
+    </DialogStackInputs>
   );
-};
+}
 
 export default StackSearchDialog
