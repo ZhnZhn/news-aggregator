988840a957c9307512245c7d05ead8cb2942c94d
Yevhenii Molodyi <eugmolody@gmail.com>
2023-08-11 13:40:40 +0300

    Add webpack.prod.js configuration

---
+++ webpack.prod.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const path = require('path')
+, webpack = require('webpack')
+, HtmlWebpackPlugin = require('html-webpack-plugin')
+, babelConfig = require('./babel.config')
+, TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  cache: true,
+  entry: {
+    app: {
+      import: path.resolve('src', 'index.jsx'),
+      dependOn: 'lib'
+    },
+    lib: [
+       "preact",
+       "preact/compat",                   
+       "dompurify",       
+       "zustand",
+       "timeago.js"              
+    ],
+  },
+  output: {
+      path: path.resolve('app'),
+      filename: "[name]_[chunkhash].js",
+      chunkFilename: "[name]_[chunkhash].js",
+      publicPath: 'app/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+             cacheDirectory: true,
+             ...babelConfig
+          }
+        },
+        include: [
+          path.join(__dirname),
+          path.join(__dirname, "src"),
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx'],
+    alias: { 
+      "react": "preact/compat",      
+      "react-dom": "preact/compat",     
+    }    
+  },
+  plugins : [
+    new HtmlWebpackPlugin({
+      minify: false,
+      filename: path.resolve('index.html'),
+      template: path.resolve('template', 'index.ejs'),
+      inject: false
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  }
+}
