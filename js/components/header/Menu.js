ff7135d5dccd454decf8ae3e767f0d34302c0355
Yevhenii Molodyi <eugmolody@gmail.com>
2023-08-14 11:17:27 +0300

    Add properties ref to header Menu function component

--- js/components/header/Menu.js
+++ js/components/header/Menu.js
@@ -6,15 +6,21 @@ exports.default = void 0;
 var _uiApi = require("../uiApi");
 var _MenuTopic = _interopRequireDefault(require("./MenuTopic"));
 var _jsxRuntime = require("preact/jsx-runtime");
+const _isArr = Array.isArray;
 const Menu = _ref => {
   let {
     refFirstItem,
+    refLastItem,
     style,
     itemStyle,
     menuModel
   } = _ref;
+  if (!_isArr(menuModel)) {
+    return null;
+  }
+  const _lastIndex = menuModel.length - 1;
   return menuModel.map((topic, index) => (0, _jsxRuntime.jsx)(_MenuTopic.default, {
-    refBt: index === 0 ? refFirstItem : void 0,
+    refBt: index === 0 ? refFirstItem : index === _lastIndex ? refLastItem : void 0,
     caption: topic.t,
     items: topic.items,
     style: style,
--- src/components/header/Menu.jsx
+++ src/components/header/Menu.jsx
@@ -1,20 +1,34 @@
 import { memo } from '../uiApi';
 import MenuTopic from './MenuTopic';
 
+const _isArr = Array.isArray;
+
 const Menu = ({
   refFirstItem,
+  refLastItem,
   style,
   itemStyle,
   menuModel
-}) => menuModel.map((topic, index) => (
-  <MenuTopic
-    key={topic.t}
-    refBt={index === 0 ? refFirstItem : void 0}
-    caption={topic.t}
-    items={topic.items}
-    style={style}
-    itemStyle={itemStyle}
-  />
-))
+}) => {
+  if (!_isArr(menuModel)) {
+    return null;
+  }
+  const _lastIndex = menuModel.length - 1;
+  return menuModel.map((topic, index) => (
+    <MenuTopic
+      key={topic.t}
+      refBt={index === 0
+       ? refFirstItem
+       : index === _lastIndex
+          ? refLastItem
+          : void 0
+      }
+      caption={topic.t}
+      items={topic.items}
+      style={style}
+      itemStyle={itemStyle}
+    />
+  ))
+}
 
 export default memo(Menu)
