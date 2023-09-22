62e1b3ac2d8372e49305c58b38862d0469532f74
Yevhenii Molodyi <eugmolody@gmail.com>
2023-09-22 11:36:48 +0300

    Add factory function fGetDialogConfig to MessariDialog

--- js/components/dialogs/MessariDialog.js
+++ js/components/dialogs/MessariDialog.js
@@ -2,44 +2,21 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
-exports["default"] = void 0;
-var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+exports.default = void 0;
 var _DialogStackInputs = _interopRequireDefault(require("./DialogStackInputs"));
 var _Links = require("../links/Links");
 var _DialogFn = require("./DialogFn");
 var _jsxRuntime = require("preact/jsx-runtime");
-var ASSET_OPTIONS = [["All", "all"]
-  /*,
-  [Bitcoin", "BTC"],
-  [Ethereum", "ETH"],
-  [Tether", "USDT"],
-  [XRP", "XRP"],
-  [Bitcoin-Cash", "BCH"],
-  ["Chainlink", "LINK"],
-  ["Cardano", "ADA"],
-  ["Bitcoin-SV", "BSV"],
-  ["Litecoin", "LTC"],
-  ["Binance-Coin", "BNB"],
-  ["Crypto-Com-Chain", "CRO"],
-  ["Tezos", "XTZ"],
-  ["EOS", "EOS"],
-  ["Stellar", "XLM"],
-  ["Monero", "XMR"],
-  ["Tron", "TRX"],
-  ["Unus-Sed-Leo", "LEO"],
-  ["USD-Coin", "USDC"],
-  ["Vechain", "VET"],
-  ["Cosmos", "ATOM"],
-  */],
-  DF_ASSET = ASSET_OPTIONS[0],
-  INPUT_CONFIGS = [['s', 'assetKey', 'News about', ASSET_OPTIONS, DF_ASSET]],
-  _crDfInputs = (0, _DialogFn.crDfInputs)(INPUT_CONFIGS),
-  INITIAL_INPUTS = _crDfInputs[0],
-  TOGGLES = _crDfInputs[1],
-  IS_INPUTS = _crDfInputs[2];
-var MessariDialog = function MessariDialog(props) {
-  var paneCaption = (0, _DialogFn.getPaneCaption)(props.itemConf);
-  return (0, _jsxRuntime.jsx)(_DialogStackInputs["default"], (0, _extends2["default"])({}, props, {
+const ASSET_OPTIONS = [["All", "all"]],
+  _crInputConfigs = () => [(0, _DialogFn.crInputSelectConfig)("assetKey", ASSET_OPTIONS, {
+    caption: "News about"
+  })],
+  _getDialogConfig = (0, _DialogFn.fGetDialogConfig)(_crInputConfigs);
+const MessariDialog = props => {
+  const paneCaption = (0, _DialogFn.getPaneCaption)(props.itemConf),
+    [INPUT_CONFIGS, INITIAL_INPUTS, TOGGLES, IS_INPUTS] = _getDialogConfig();
+  return (0, _jsxRuntime.jsx)(_DialogStackInputs.default, {
+    ...props,
     caption: paneCaption,
     loadId: "MS",
     INPUT_CONFIGS: INPUT_CONFIGS,
@@ -47,8 +24,8 @@ var MessariDialog = function MessariDialog(props) {
     TOGGLES: TOGGLES,
     IS_INPUTS: IS_INPUTS,
     children: (0, _jsxRuntime.jsx)(_Links.MessariLink, {})
-  }));
+  });
 };
 var _default = MessariDialog;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=MessariDialog.js.map
--- src/components/dialogs/MessariDialog.jsx
+++ src/components/dialogs/MessariDialog.jsx
@@ -1,46 +1,27 @@
 import DialogStackInputs from './DialogStackInputs';
 import { MessariLink } from '../links/Links';
 import {
-  crDfInputs,
+  crInputSelectConfig,
+  fGetDialogConfig,
   getPaneCaption
 } from './DialogFn';
 
 const ASSET_OPTIONS = [
 ["All", "all"]
-/*,
-[Bitcoin", "BTC"],
-[Ethereum", "ETH"],
-[Tether", "USDT"],
-[XRP", "XRP"],
-[Bitcoin-Cash", "BCH"],
-["Chainlink", "LINK"],
-["Cardano", "ADA"],
-["Bitcoin-SV", "BSV"],
-["Litecoin", "LTC"],
-["Binance-Coin", "BNB"],
-["Crypto-Com-Chain", "CRO"],
-["Tezos", "XTZ"],
-["EOS", "EOS"],
-["Stellar", "XLM"],
-["Monero", "XMR"],
-["Tron", "TRX"],
-["Unus-Sed-Leo", "LEO"],
-["USD-Coin", "USDC"],
-["Vechain", "VET"],
-["Cosmos", "ATOM"],
-*/]
-, DF_ASSET = ASSET_OPTIONS[0]
-, INPUT_CONFIGS = [
-  ['s','assetKey','News about', ASSET_OPTIONS, DF_ASSET]
 ]
-, [
-  INITIAL_INPUTS,
-  TOGGLES,
-  IS_INPUTS
-] = crDfInputs(INPUT_CONFIGS);
+, _crInputConfigs = () => [
+  crInputSelectConfig("assetKey", ASSET_OPTIONS, {caption: "News about"})
+]
+, _getDialogConfig = fGetDialogConfig(_crInputConfigs);
 
 const MessariDialog = (props) => {
-  const paneCaption = getPaneCaption(props.itemConf);
+  const paneCaption = getPaneCaption(props.itemConf)
+  , [
+    INPUT_CONFIGS,
+    INITIAL_INPUTS,
+    TOGGLES,
+    IS_INPUTS
+  ] = _getDialogConfig();
   return (
     <DialogStackInputs
       {...props}
