58834851081fa18bc8094ad758eb2ebd693d4e88
Yevhenii Molodyi <eugmolody@gmail.com>
2023-09-26 11:32:36 +0300

    Remove unused named exports from PoweredByLink

--- js/components/links/PoweredByLink.js
+++ js/components/links/PoweredByLink.js
@@ -2,15 +2,9 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
-exports.PoweredByWebzLink = exports.PoweredByTheNewsApi = exports.PoweredByStackOverflow = exports.PoweredByNewsApi = void 0;
+exports.PoweredByWebzLink = exports.PoweredByNewsApi = void 0;
 var _fPoweredBy = _interopRequireDefault(require("./fPoweredBy"));
 var _Links = require("./Links");
-var PoweredByNewsApi = (0, _fPoweredBy["default"])(_Links.NewsApiLink);
-exports.PoweredByNewsApi = PoweredByNewsApi;
-var PoweredByTheNewsApi = (0, _fPoweredBy["default"])(_Links.TheNewsApiLink);
-exports.PoweredByTheNewsApi = PoweredByTheNewsApi;
-var PoweredByStackOverflow = (0, _fPoweredBy["default"])(_Links.StackOverflowLink);
-exports.PoweredByStackOverflow = PoweredByStackOverflow;
-var PoweredByWebzLink = (0, _fPoweredBy["default"])(_Links.WebzLink);
-exports.PoweredByWebzLink = PoweredByWebzLink;
+const PoweredByNewsApi = exports.PoweredByNewsApi = (0, _fPoweredBy.default)(_Links.NewsApiLink);
+const PoweredByWebzLink = exports.PoweredByWebzLink = (0, _fPoweredBy.default)(_Links.WebzLink);
 //# sourceMappingURL=PoweredByLink.js.map
