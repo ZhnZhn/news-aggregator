5dc97814b6394be2b55c4b124b8eac5d51183396
Yevhenii Molodyi <eugmolody@gmail.com>
2023-12-26 15:42:24 +0200

    Add HAS_KEYBOARD_FOCUS to useKeyEscape

--- js/components/hooks/useKeyEscape.js
+++ js/components/hooks/useKeyEscape.js
@@ -1,18 +1,21 @@
 "use strict";
 
 exports.__esModule = true;
-exports["default"] = void 0;
+exports.default = void 0;
 var _uiApi = require("../uiApi");
-/*eslint-disable react-hooks/exhaustive-deps */
-var useKeyEscape = function useKeyEscape(onKeyEscape) {
-  return (0, _uiApi.useCallback)(function (evt) {
-    if (evt && evt.keyCode === 27) {
-      onKeyEscape();
+var _has = require("../has");
+const useKeyEscape = (onKeyEscape, isPropagation) => _has.HAS_KEYBOARD_FOCUS
+/*eslint-disable react-hooks/exhaustive-deps*/
+/*eslint-disable react-hooks/rules-of-hooks*/ ? (0, _uiApi.useCallback)(evt => {
+  if (evt && evt.keyCode === 27) {
+    onKeyEscape();
+    if (!isPropagation) {
+      evt.stopPropagation();
     }
-  }, []);
-};
+  }
+}, []) : _uiApi.FN_NOOP;
 // onKeyEscape
 /*eslint-enable react-hooks/exhaustive-deps */
-var _default = useKeyEscape;
-exports["default"] = _default;
+/*eslint-enable react-hooks/rules-of-hooks*/
+var _default = exports.default = useKeyEscape;
 //# sourceMappingURL=useKeyEscape.js.map
--- src/components/hooks/useKeyEscape.js
+++ src/components/hooks/useKeyEscape.js
@@ -1,12 +1,26 @@
-import { useCallback } from '../uiApi';
+import {
+  FN_NOOP,
+  useCallback
+} from '../uiApi';
+import { HAS_KEYBOARD_FOCUS } from '../has';
 
-/*eslint-disable react-hooks/exhaustive-deps */
-const useKeyEscape = onKeyEscape => useCallback(evt => {
-  if (evt && evt.keyCode === 27) {
-    onKeyEscape()
-  }
-}, [])
+const useKeyEscape = (
+  onKeyEscape,
+  isPropagation
+) => HAS_KEYBOARD_FOCUS
+/*eslint-disable react-hooks/exhaustive-deps*/
+/*eslint-disable react-hooks/rules-of-hooks*/
+? useCallback(evt => {
+    if (evt && evt.keyCode === 27) {
+      onKeyEscape()
+      if (!isPropagation) {
+        evt.stopPropagation()
+      }
+    }
+  }, [])
+: FN_NOOP;
 // onKeyEscape
 /*eslint-enable react-hooks/exhaustive-deps */
+/*eslint-enable react-hooks/rules-of-hooks*/
 
 export default useKeyEscape
