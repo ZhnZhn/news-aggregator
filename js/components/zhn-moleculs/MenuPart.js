3dc278f3db568fac2920b18e9a88781e74c79520
Yevhenii Molodyi <eugmolody@gmail.com>
2024-03-27 11:53:43 +0200

    Add safeMap function from uiApi to MenuPart function component

--- js/components/zhn-moleculs/MenuPart.js
+++ js/components/zhn-moleculs/MenuPart.js
@@ -8,37 +8,30 @@ var _crStyle = require("../crStyle");
 var _OpenClose = _interopRequireDefault(require("../zhn-atoms/OpenClose"));
 var _MenuItem = _interopRequireDefault(require("../zhn-atoms/MenuItem"));
 var _jsxRuntime = require("preact/jsx-runtime");
-const _renderMenuItems = option => {
-  const {
-    items,
-    hmItems,
-    onClick,
-    ...restItemProps
-  } = option;
-  return (items || []).map((item, index) => {
-    const _itemConf = {
-      ...(hmItems || {})[item.id],
-      ...restItemProps
-    };
-    return (0, _jsxRuntime.jsx)(_MenuItem.default, {
-      className: _crStyle.CL_ROW_NEWS_SOURCE,
-      caption: _itemConf.menuTitle,
-      onClick: (0, _uiApi.bindTo)(onClick, _itemConf)
-    }, index);
-  });
-};
 const MenuPart = _ref => {
   let {
     caption,
     isInitClose,
-    ...restProps
+    items,
+    hmItems,
+    onClick,
+    ...restItemProps
   } = _ref;
   return (0, _jsxRuntime.jsx)(_OpenClose.default, {
     caption: caption,
     isClose: isInitClose,
-    children: _renderMenuItems(restProps)
+    children: (0, _uiApi.safeMap)(items, (item, index) => {
+      const _itemConf = {
+        ...(hmItems || {})[item.id],
+        ...restItemProps
+      };
+      return (0, _jsxRuntime.jsx)(_MenuItem.default, {
+        className: _crStyle.CL_ROW_NEWS_SOURCE,
+        caption: _itemConf.menuTitle,
+        onClick: (0, _uiApi.bindTo)(onClick, _itemConf)
+      }, index);
+    })
   });
 };
-var _default = MenuPart;
-exports.default = _default;
+var _default = exports.default = MenuPart;
 //# sourceMappingURL=MenuPart.js.map
--- src/components/zhn-moleculs/MenuPart.jsx
+++ src/components/zhn-moleculs/MenuPart.jsx
@@ -1,48 +1,40 @@
-import { bindTo } from '../uiApi';
+import {
+  bindTo,
+  safeMap
+} from '../uiApi';
 
 import { CL_ROW_NEWS_SOURCE } from '../crStyle';
 
 import OpenClose from '../zhn-atoms/OpenClose';
 import MenuItem from '../zhn-atoms/MenuItem';
 
-
-const _renderMenuItems = (
-  option
-) => {
-  const {
-    items,
-    hmItems,
-    onClick,
-    ...restItemProps
-  } = option;
-  return (items || []).map((item, index) => {
-    const _itemConf = {
-      ...(hmItems || {})[item.id],
-      ...restItemProps
-    };
-
-    return (
-      <MenuItem
-        key={index}
-        className={CL_ROW_NEWS_SOURCE}
-        caption={_itemConf.menuTitle}
-        onClick={bindTo(onClick, _itemConf)}
-      />
-    );
-  })
-}
-
 const MenuPart = ({
   caption,
   isInitClose,
-  ...restProps
+  items,
+  hmItems,
+  onClick,
+  ...restItemProps
 }) => (
   <OpenClose
      caption={caption}
      isClose={isInitClose}
   >
-     {_renderMenuItems(restProps)}
+     {safeMap(items, (item, index) => {
+         const _itemConf = {
+           ...(hmItems || {})[item.id],
+           ...restItemProps
+         };
+         return (
+           <MenuItem
+             key={index}
+             className={CL_ROW_NEWS_SOURCE}
+             caption={_itemConf.menuTitle}
+             onClick={bindTo(onClick, _itemConf)}
+           />
+         );
+     })}
   </OpenClose>
-)
+);
 
 export default MenuPart
