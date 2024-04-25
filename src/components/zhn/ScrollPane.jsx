3ecad528e1e12e2ebc872dbc7979145fba1fada4
Yevhenii Molodyi <eugmolody@gmail.com>
2024-04-25 16:14:06 +0300

    Add refScrollPane property to ScrollPane function component

--- src/components/zhn/ScrollPane.jsx
+++ src/components/zhn/ScrollPane.jsx
@@ -1,11 +1,13 @@
 import { crWithScrollCn } from '../crStyle';
 
 const ScrollPane = ({
+  refScrollPane,
   style,
   className,
   children
 }) => (
   <div
+    ref={refScrollPane}
     className={crWithScrollCn(className)}
     style={style}
   >
