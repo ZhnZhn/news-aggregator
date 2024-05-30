60473e9e1dc77539e3f2a63a2673c5805c361843
Yevhenii Molodyi <eugmolody@gmail.com>
2024-05-30 11:56:32 +0300

    Edit Dialog.Style

--- js/components/dialogs/Dialog.Style.js
+++ js/components/dialogs/Dialog.Style.js
@@ -2,8 +2,8 @@
 
 exports.__esModule = true;
 exports.crInputSelectStyle = exports.S_POWERED_BY = exports.S_INPUT_ROOT = exports.S_INPUT_DATE = exports.S_DIALOG_CAPTION = exports.S_BT_RAISED = exports.S_BROWSER_CAPTION = void 0;
-const INPUT_WIDTH = 'calc(190px + 4rem)';
 const S_DIALOG_CAPTION = exports.S_DIALOG_CAPTION = {
+  margin: -1,
   fontSize: '1.125rem'
 };
 const S_BROWSER_CAPTION = exports.S_BROWSER_CAPTION = {
@@ -14,16 +14,17 @@ const S_BT_RAISED = exports.S_BT_RAISED = {
   marginRight: 2,
   marginLeft: 2
 };
-const S_INPUT_ROOT = exports.S_INPUT_ROOT = {
-  width: INPUT_WIDTH,
-  display: 'block'
-};
 const S_INPUT_DATE = exports.S_INPUT_DATE = {
   display: 'inline-block',
   width: 110
 };
 const S_POWERED_BY = exports.S_POWERED_BY = {
   margin: '16px 0 8px 16px'
+};
+const INPUT_WIDTH = 'calc(190px + 4rem)';
+const S_INPUT_ROOT = exports.S_INPUT_ROOT = {
+  display: 'block',
+  width: INPUT_WIDTH
 };
 const crInputSelectStyle = function (width) {
   if (width === void 0) {
--- src/components/dialogs/Dialog.Style.js
+++ src/components/dialogs/Dialog.Style.js
@@ -1,6 +1,5 @@
-const INPUT_WIDTH = 'calc(190px + 4rem)';
-
 export const S_DIALOG_CAPTION = {
+  margin: -1,
   fontSize: '1.125rem'
 }
 export const S_BROWSER_CAPTION = {
@@ -13,10 +12,6 @@ export const S_BT_RAISED = {
   marginLeft: 2
 }
 
-export const S_INPUT_ROOT = {
-  width: INPUT_WIDTH,
-  display: 'block'
-}
 export const S_INPUT_DATE = {
   display: 'inline-block',
   width: 110
@@ -24,6 +19,12 @@ export const S_INPUT_DATE = {
 export const S_POWERED_BY = {
   margin: '16px 0 8px 16px'
 };
+
+const INPUT_WIDTH = 'calc(190px + 4rem)';
+export const S_INPUT_ROOT = {
+  display: 'block',
+  width: INPUT_WIDTH,
+}
 
 export const crInputSelectStyle = (
   width=INPUT_WIDTH
