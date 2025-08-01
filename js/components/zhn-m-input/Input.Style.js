93a2447bfb59dba9c56f77f4a3c5d46fbceef154
Yevhenii Molodyi <eugmolody@gmail.com>
2025-08-01 17:08:59 +0300

    Edit css

--- js/components/zhn-m-input/Input.Style.js
+++ js/components/zhn-m-input/Input.Style.js
@@ -1,13 +1,12 @@
 "use strict";
 
 exports.__esModule = true;
-exports.S_BT_ARROW_RIGHT = exports.CL_TEXTFIELD_INPUT_DIV = exports.CL_TEXTFIELD_INPUT = exports.CL_SELECT_VALUE = exports.CL_SELECT_OPTIONS = exports.CL_SELECT_LABEL = exports.CL_SELECT_ITEM = exports.CL_SELECT_INPUT_LINE = exports.CL_SELECT_DIV_VALUE = exports.CL_SELECT_DIV_BT = exports.CL_SELECT_DIV = exports.CL_SELECT = exports.CL_INPUT_MSG_ERR = exports.CL_INPUT_LINE = exports.CL_BT_SVG_CLEAR = void 0;
+exports.S_BT_ARROW_RIGHT = exports.CL_TEXTFIELD_INPUT_DIV = exports.CL_TEXTFIELD_INPUT = exports.CL_SELECT_VALUE = exports.CL_SELECT_OPTIONS = exports.CL_SELECT_LABEL = exports.CL_SELECT_ITEM = exports.CL_SELECT_INPUT_LINE = exports.CL_SELECT_DIV_BT = exports.CL_SELECT_DIV = exports.CL_SELECT = exports.CL_INPUT_MSG_ERR = exports.CL_INPUT_LINE = exports.CL_BT_SVG_CLEAR = void 0;
 var _crStyle = require("../crStyle");
 const CL_SELECT = exports.CL_SELECT = 'm-select',
   CL_SELECT_LABEL = exports.CL_SELECT_LABEL = `${CL_SELECT}__label`,
   CL_SELECT_VALUE = exports.CL_SELECT_VALUE = `${CL_SELECT}__value`,
   CL_SELECT_DIV = exports.CL_SELECT_DIV = `${CL_SELECT}__div`,
-  CL_SELECT_DIV_VALUE = exports.CL_SELECT_DIV_VALUE = `${CL_SELECT}__div__value`,
   CL_SELECT_DIV_BT = exports.CL_SELECT_DIV_BT = `${CL_SELECT}__div__bt`,
   CL_SELECT_INPUT_LINE = exports.CL_SELECT_INPUT_LINE = `${CL_SELECT}__line`,
   CL_SELECT_OPTIONS = exports.CL_SELECT_OPTIONS = (0, _crStyle.crWithScrollCn)(`${CL_SELECT}__options`),
--- src/components/zhn-m-input/Input.Style.js
+++ src/components/zhn-m-input/Input.Style.js
@@ -4,7 +4,6 @@ export const CL_SELECT = 'm-select'
 , CL_SELECT_LABEL = `${CL_SELECT}__label`
 , CL_SELECT_VALUE = `${CL_SELECT}__value`
 , CL_SELECT_DIV = `${CL_SELECT}__div`
-, CL_SELECT_DIV_VALUE = `${CL_SELECT}__div__value`
 , CL_SELECT_DIV_BT = `${CL_SELECT}__div__bt`
 , CL_SELECT_INPUT_LINE = `${CL_SELECT}__line`
 , CL_SELECT_OPTIONS = crWithScrollCn(`${CL_SELECT}__options`)
