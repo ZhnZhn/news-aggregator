03ef5300d77e5ebfa58f2417935a7e732f9d2676
Yevhenii Molodyi <eugmolody@gmail.com>
2026-04-14 14:33:14 +0300

    Reduce size of function component SvgDelete

--- js/components/zhn/svg/SvgDelete.js
+++ js/components/zhn/svg/SvgDelete.js
@@ -8,7 +8,7 @@ var _jsxRuntime = require("preact/jsx-runtime");
 const SvgDelete = props => (0, _jsxRuntime.jsxs)(_SvgIcon.default, {
   ...props,
   children: [(0, _jsxRuntime.jsx)("path", {
-    d: "M10 5a2 2 0 0 0-1.34.51l-6.32 5.74a1 1 0 0 0 0 1.48l6.32 5.74A2 2 0 0 0 10 19h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z"
+    d: "M10 5a2 2 0 0 0-1.3.5l-6.3 5.7a1 1 0 0 0 0 1.4l6.3 5.7A2 2 0 0 0 10 19h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z"
   }), (0, _jsxRuntime.jsx)("path", {
     d: "m12 9 6 6"
   }), (0, _jsxRuntime.jsx)("path", {
--- src/components/zhn/svg/SvgDelete.jsx
+++ src/components/zhn/svg/SvgDelete.jsx
@@ -2,7 +2,7 @@ import SvgIcon from './SvgIcon';
 
 const SvgDelete = (props) => (
   <SvgIcon {...props}>
-    <path d="M10 5a2 2 0 0 0-1.34.51l-6.32 5.74a1 1 0 0 0 0 1.48l6.32 5.74A2 2 0 0 0 10 19h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z" />
+    <path d="M10 5a2 2 0 0 0-1.3.5l-6.3 5.7a1 1 0 0 0 0 1.4l6.3 5.7A2 2 0 0 0 10 19h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z" />
     <path d="m12 9 6 6" />
     <path d="m18 9-6 6"/>
   </SvgIcon>
